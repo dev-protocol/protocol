812ca5cac46d71b373f4633128d418cf0de46b92
aggre <hiroyuki.aggre@gmail.com>
2019-03-17 08:47:53 +0900

    re-apply truffle unbox

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,15 +1,15 @@
-pragma solidity >=0.4.0;
+pragma solidity >=0.4.25<0.6.0;
 
 contract Migrations {
 	address public owner;
 	uint public last_completed_migration;
 
-	constructor() public {
-		owner = msg.sender;
-	}
-
 	modifier restricted() {
 		if (msg.sender == owner) _;
+	}
+
+	constructor() public {
+		owner = msg.sender;
 	}
 
 	function setCompleted(uint completed) public restricted {
