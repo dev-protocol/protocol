b2fd3780630ea5bffa5bb776e10b1ae64803e5e3
aggre <hiroyuki.aggre@gmail.com>
2019-04-26 00:18:49 +0900

    fix solium error

--- contracts/libs/UintToString.sol
+++ contracts/libs/UintToString.sol
@@ -1,15 +1,12 @@
 pragma solidity ^0.5.0;
 
 library UintToString {
-	function toString(uint _i)
-		internal
-		pure
-		returns (string memory _uintAsString)
-	{
-		if (_i == 0) {
+	function toString(uint _i) internal pure returns (string memory _string) {
+		uint256 i = _i;
+		if (i == 0) {
 			return "0";
 		}
-		uint j = _i;
+		uint j = i;
 		uint len;
 		while (j != 0) {
 			len++;
@@ -17,9 +14,9 @@ library UintToString {
 		}
 		bytes memory bstr = new bytes(len);
 		uint k = len - 1;
-		while (_i != 0) {
-			bstr[k--] = byte(uint8(48 + _i % 10));
-			_i /= 10;
+		while (i != 0) {
+			bstr[k--] = byte(uint8(48 + i % 10));
+			i /= 10;
 		}
 		return string(bstr);
 	}
