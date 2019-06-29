6609d3ff4ad34d29ffbbd4a52581d461661154be
aggre <hiroyuki.aggre@gmail.com>
2019-06-29 13:54:10 +0900

    update test cases for distributor

--- test/distributor.ts
+++ test/distributor.ts
@@ -1,37 +1,69 @@
 contract('Distributor', () => {
-	describe('Oraclize', () => {
-		it('Send Oraclize queries as soon as it is created')
+	describe('setMintVolumePerDay', () => {
+		it('Change the value of mint volume per day')
 
 		it(
-			'Should fail to all Oraclize queries when the ETH balance of this contract is insufficient'
+			'Should fail to change the value of mint volume per day when sent from the non-owner account'
 		)
-
-		it('When failed to Oraclize, self-destruct the creator as an address')
-
-		it('Oraclize gets the downloads count of each npm packages')
 	})
 
-	describe('Payout', () => {
-		it(
-			"Distribution token's total supply increases after `payout` function are executed"
-		)
+	describe('distribute', () => {
+		describe('Fetch the number of downloads', () => {
+			it('Fetch the specify Repository Contract mapped OSS number of downloads')
 
-		it(
-			"Repository Contract's `total` and `price` increases after `payout` function are executed"
-		)
+			it(
+				'Should fail to fetch the number of downloads when not exists Repository Contract'
+			)
+		})
 
-		it('Destination of newly issued part is each repository contract')
+		describe('Target period', () => {
+			it('Target period is from the last run to yesterday')
 
-		it(
-			'Distributions per repository contract is equal to "each_packages_downloads / all_packages_downloads * total_distributions"'
-		)
+			it('Should fail to re-run if within one day from the last run date')
+		})
 
-		it(
-			'When all packages payout is done, self-destruct this contract and send ETH to the creator of this contract'
-		)
+		describe('Increment dividends', () => {
+			it(
+				`
+				'totalDownloadsPerDay' is 100,
+				the target period is 1 day,
+				the previous download count is x,
+				the current download count is y,
+				and 'mintVolumePerDay' is 5;
+				the result is y ÷ (100 - x + y) * 5`
+			)
 
-		it('Should fail to payout already executed')
+			it(
+				"When after increment, change the value of 'totalDownloadsPerDay' is (100 - x + y)"
+			)
+		})
 
-		it('Force execute payout to all repository contracts')
+		describe('Timestamp', () => {
+			it('Change the value of seconds per block')
+
+			it(
+				'Should fail to change the value of seconds per block when sent from the non-owner account'
+			)
+		})
+
+		it('The sent ETH will be returned to the sender')
+	})
+
+	describe('withdraw', () => {
+		describe('Withdraw is mint', () => {
+			it('Withdraw mints an ERC20 token specified in the State Contract')
+		})
+
+		describe('Withdrawable amount', () => {
+			it(
+				'The withdrawable amount each holder is the number multiplied the balance of the price per Repository Contract and the Repository Contract of the sender'
+			)
+
+			it(
+				'The withdrawal amount is always the full amount of the withdrawable amount'
+			)
+
+			it('When the withdrawable amount is 0, the withdrawal amount is 0')
+		})
 	})
 })
