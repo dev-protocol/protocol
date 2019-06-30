2180a855918e8075aed1aa25d9222737159a269f
aggre <hiroyuki.aggre@gmail.com>
2019-06-30 10:28:26 +0900

    add test for repository

---
+++ test/reposiory.ts
@@ -0,0 +1,11 @@
+contract('Reposiory', () => {
+	describe('Package name', () => {
+		it('Get a mapped package name')
+	})
+
+	describe('Transfer', () => {
+		it(
+			"Execute the Distributor Contract's 'beforeBalanceChange' function before the 'transfer' function changes the balance"
+		)
+	})
+})
