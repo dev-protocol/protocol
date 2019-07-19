93539da3786ce6b97f3d01e1bba5df27f9a5316c
narita1980 <238369+narita1980@users.noreply.github.com>
2019-07-19 11:37:59 +0900

    update test Repository:getPackage. (#17)
    * update test Repository:getPackage.
    
    *  I ran npm run lint

--- test/repository.ts
+++ test/repository.ts
@@ -1,6 +1,21 @@
-contract('Reposiory', () => {
+contract('Reposiory', ([deployer]) => {
+	const repositoryContract = artifacts.require('Repository')
+
 	describe('getPackage', () => {
-		it('Get a mapped package name')
+		it('Get a mapped package name', async () => {
+			const repository = await repositoryContract.new(
+				'pkg',
+				'pkg_token',
+				'PKG',
+				18,
+				10000,
+				{
+					from: deployer
+				}
+			)
+			const results = await repository.package()
+			expect(results.toString()).to.be.equal('pkg')
+		})
 	})
 
 	describe('transfer', () => {
