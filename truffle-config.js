87be93b11a7a4e6118fb5cf40b92a46f679b568f
aggre <hiroyuki.aggre@gmail.com>
2019-08-03 10:59:20 +0900

    add azure network

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,38 @@
-/* eslint-disable @typescript-eslint/camelcase */
+/* eslint-disable @typescript-eslint/camelcase, @typescript-eslint/no-var-requires */
 require('ts-node/register')
+require('dotenv').config()
+const Web3 = require('web3')
+
+const {
+	WEB3_PROVIDERS_AZURE,
+	WEB3_PROVIDERS_AZURE_ACCOUNT,
+	WEB3_PROVIDERS_AZURE_PASSWORD
+} = process.env
+
+const createProvider = (node, account, password) => {
+	const prov = new Web3.providers.HttpProvider(node)
+	const web3 = new Web3(prov)
+	web3.eth.personal.unlockAccount(account, password)
+	return prov
+}
 
 module.exports = {
 	test_file_extension_regexp: /.*\.ts$/,
 	compilers: {
 		solc: {
 			version: '^0.5.9'
+		}
+	},
+	networks: {
+		azure: {
+			provider: () =>
+				createProvider(
+					WEB3_PROVIDERS_AZURE,
+					WEB3_PROVIDERS_AZURE_ACCOUNT,
+					WEB3_PROVIDERS_AZURE_PASSWORD
+				),
+			network_id: '*',
+			from: WEB3_PROVIDERS_AZURE_ACCOUNT
 		}
 	}
 }
