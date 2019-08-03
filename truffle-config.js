de793dde315b5b3eba9b7a686897bb7b694073ae
aggre <hiroyuki.aggre@gmail.com>
2019-08-04 02:36:56 +0900

    Deploy to azure (#25)
    * add dotenv
    
    * add azure network
    
    * install additional packages
    
    * implement migrate to azure
    
    * remove unnecessary command
    
    * add a command to deploy contracts
    
    * add example file for .env
    
    * bugfix: will error when not exist .env file

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,39 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/camelcase */
 require('ts-node/register')
+require('dotenv').config()
+const Web3 = require('web3')
+const {
+	WEB3_PROVIDERS_AZURE,
+	WEB3_PROVIDERS_AZURE_FROM,
+	WEB3_PROVIDERS_AZURE_ACCOUNT_PASSPHRASE
+} = process.env
+const createProvider = (node, account, password) => {
+	const prov = new Web3.providers.HttpProvider(node)
+	const web3 = new Web3(prov)
+	web3.eth.personal.unlockAccount(account, password)
+	return prov
+}
 
 module.exports = {
 	test_file_extension_regexp: /.*\.ts$/,
 	compilers: {
 		solc: {
 			version: '^0.5.9'
+		}
+	},
+	networks: {
+		azure: {
+			provider: () =>
+				createProvider(
+					WEB3_PROVIDERS_AZURE,
+					WEB3_PROVIDERS_AZURE_FROM,
+					WEB3_PROVIDERS_AZURE_ACCOUNT_PASSPHRASE
+				),
+			from: WEB3_PROVIDERS_AZURE_FROM,
+			network_id: '*',
+			gas: 0,
+			gasPrice: 0
 		}
 	}
 }
