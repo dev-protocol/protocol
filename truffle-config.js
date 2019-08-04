0da729214f803a9bb3e932d9334ab819078798e7
aggre <hiroyuki.aggre@gmail.com>
2019-08-04 21:16:30 +0900

    rename environment vars

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,7 @@
 require('ts-node/register')
 require('dotenv').config()
 const HDWalletProvider = require('truffle-hdwallet-provider')
-const {WEB3_PROVIDERS_AZURE, WEB3_WALLET_MNEMONIC} = process.env
+const {ETHEREUM_PROVIDERS_AZURE, ETHEREUM_WALLET_MNEMONIC} = process.env
 
 module.exports = {
 	test_file_extension_regexp: /.*\.ts$/,
@@ -15,7 +15,10 @@ module.exports = {
 	networks: {
 		azure: {
 			provider: () =>
-				new HDWalletProvider(WEB3_WALLET_MNEMONIC, WEB3_PROVIDERS_AZURE),
+				new HDWalletProvider(
+					ETHEREUM_WALLET_MNEMONIC,
+					ETHEREUM_PROVIDERS_AZURE
+				),
 			network_id: '*',
 			gas: 0,
 			gasPrice: 0
