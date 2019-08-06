b6f7399f617ac788ce094e0dc524ab1b36b186ff
theMistletoe <katachiwotodomete@gmail.com>
2019-08-06 21:09:01 +0900

    fix: change space to tab

--- contracts/State.test.sol
+++ contracts/State.test.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 import "./State.sol";
 
 contract StateTest is State {
-    function containsMarket(address _market) public view returns (bool) {
-        return markets[_market];
-    }
+	function containsMarket(address _market) public view returns (bool) {
+		return markets[_market];
+	}
 }
