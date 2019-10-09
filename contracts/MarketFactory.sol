8b4346a5a6881380e09df54b6da573015ef01d2a
Motoaki Tanaka <katachiwotodomete@gmail.com>
2019-10-10 07:49:44 +0900

    MarketFactory have better emits Event instead of return value. (#35)
    * catch up for eslint
    
    * add: MarketFactory emits Create Event for telling created market
    address.

--- contracts/MarketFactory.sol
+++ contracts/MarketFactory.sol
@@ -4,10 +4,12 @@ import "./UseState.sol";
 import "./Market.sol";
 
 contract MarketFactory is UseState {
+	event Create(address indexed _from, address _market);
+
 	function createMarket(address _addr) public returns (address) {
 		Market market = new Market(_addr, false);
 		address marketAddr = address(market);
 		addMarket(marketAddr);
-		return marketAddr;
+		emit Create(msg.sender, marketAddr);
 	}
 }
--- test/market-factory.ts
+++ test/market-factory.ts
@@ -1,7 +1,47 @@
-contract('MarketFactory', () => {
-	describe('createMarket', () => {
-		it('Create a new Market Contract')
+import {
+	MarketFactoryInstance,
+	StateTestInstance,
+	MarketInstance
+} from '../types/truffle-contracts'
 
-		it('Adds a new Market Contract address to State Contract')
+contract('MarketFactory', ([deployer, u1]) => {
+	const marketFactoryContract = artifacts.require('MarketFactory')
+	const marketContract = artifacts.require('Market')
+	const stateContract = artifacts.require('StateTest')
+
+	describe('createMarket', () => {
+		var marketFactory: MarketFactoryInstance
+		var state: StateTestInstance
+		var expectedMarketAddress: any
+		var deployedMarket: MarketInstance
+
+		beforeEach(async () => {
+			state = await stateContract.new({from: deployer})
+			marketFactory = await marketFactoryContract.new({from: deployer})
+			await state.setMarketFactory(marketFactory.address, {from: deployer})
+			await marketFactory.changeStateAddress(state.address, {from: deployer})
+
+			const result = await marketFactory.createMarket(u1, {from: deployer})
+
+			expectedMarketAddress = await result.logs.filter(
+				e => e.event === 'Create'
+			)[0].args._market
+		})
+
+		it('Create a new Market Contract and emit Create Event telling created market address', async () => {
+			// eslint-disable-next-line @typescript-eslint/await-thenable
+			deployedMarket = await marketContract.at(expectedMarketAddress)
+			const behaviorAddress = await deployedMarket.behavior({from: deployer})
+
+			expect(behaviorAddress).to.be.equal(u1)
+		})
+
+		it('Adds a new Market Contract address to State Contract', async () => {
+			const isContained = await state.containsMarket(expectedMarketAddress, {
+				from: deployer
+			})
+
+			expect(isContained).to.be.equal(true)
+		})
 	})
 })
