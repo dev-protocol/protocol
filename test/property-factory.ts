869d31cbde3c9f3072a29b8952987ace0e2c84d4
theMistletoe <katachiwotodomete@gmail.com>
2019-10-09 22:32:29 +0900

    fix: change test description

--- test/property-factory.ts
+++ test/property-factory.ts
@@ -1,6 +1,8 @@
 contract('PropertyFactory', () => {
 	describe('createProperty', () => {
-		it('Create a new Property Contract')
+		it(
+			'Create a new Property Contract and emit Create Event telling created property address'
+		)
 
 		it('Adds a new Property Contract address to State Contract')
 	})
