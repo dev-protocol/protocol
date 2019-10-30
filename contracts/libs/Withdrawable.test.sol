a44e3db66e21fb4cf36669c0f34e11f1b10cee27
Akira-Taniguchi <einstein19821108>
2019-10-30 15:54:27 +0900

    add test case

--- contracts/libs/Withdrawable.test.sol
+++ contracts/libs/Withdrawable.test.sol
@@ -1,9 +1,17 @@
 pragma solidity ^0.5.0;
 
 import "./Withdrawable.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract WithdrawableTest is Withdrawable {
+	using SafeMath for uint256;
 	function t_increment(address _token, uint256 value) public {
 		increment(_token, value);
+	}
+	function t_total(address _token) public view returns (uint256){
+		return totals[_token];
+	}
+	function t_price(address _token) public view returns (uint256){
+		return prices[_token];
 	}
 }
