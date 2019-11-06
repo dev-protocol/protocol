1f01daf0917bde697e19f8f89505a69717b37f97
Akira-Taniguchi <einstein19821108>
2019-11-06 14:29:22 +0900

    add debug log

--- test/libs/Timebased.ts
+++ test/libs/Timebased.ts
@@ -3,17 +3,22 @@ contract('TimebasedTest', () => {
 	describe('TimebasedTest; t_timestamp', () => {
 		it('t_timestamp', async () => {
 			const timebasedTest = await timebasedTestContract.new()
-			const blockNumber = await timebasedTest.t_blockNumber()
-			const secondsPerBlock = await timebasedTest.t_secondsPerBlock()
-			const blockHeight = await timebasedTest.t_blockHeight()
 			const timestamp = await timebasedTest.t_timestamp()
-			const diff = blockNumber - blockHeight
-			const sec = diff * secondsPerBlock
-			console.log(sec)
-			const time = Number(await timebasedTest.t_time())
-			console.log(time)
-			const currentTime = time + sec
-			expect(timestamp.toNumber()).to.be.equal(currentTime)
+			const timestamp2 = await timebasedTest.t_timestamp()
+			console.log(timestamp)
+			console.log(timestamp2)
+			// Const timebasedTest = await timebasedTestContract.new()
+			// const blockNumber = await timebasedTest.t_blockNumber()
+			// const secondsPerBlock = await timebasedTest.t_secondsPerBlock()
+			// const blockHeight = await timebasedTest.t_blockHeight()
+			// const timestamp = await timebasedTest.t_timestamp()
+			// const diff = blockNumber - blockHeight
+			// const sec = diff * secondsPerBlock
+			// console.log(sec)
+			// const time = Number(await timebasedTest.t_time())
+			// console.log(time)
+			// const currentTime = time + sec
+			// expect(timestamp.toNumber()).to.be.equal(currentTime)
 		})
 	})
 })
