ce2809d32c32d918e5fda0552dceaf44235d00b0
Akira-Taniguchi <einstein19821108>
2019-11-12 12:18:31 +0900

    deal lint

--- contracts/DevLockUp.sol
+++ contracts/DevLockUp.sol
@@ -4,7 +4,7 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "./UseState.sol";
 
-contract DevLockUp is UseState{
+contract DevLockUp is UseState {
 	using SafeMath for uint256;
 	DevValue private devValue;
 	CanceledLockUpFlg private canceledFlg;
@@ -23,10 +23,7 @@ contract DevLockUp is UseState{
 		);
 		ERC20 devToken = ERC20(getToken());
 		uint256 balance = devToken.balanceOf(msg.sender);
-		require(
-			value <= balance,
-			"insufficient balance"
-		);
+		require(value <= balance, "insufficient balance");
 		devToken.transfer(propertyAddress, value);
 		devValue.set(propertyAddress, value);
 	}
@@ -51,10 +48,16 @@ contract DevValue {
 	using SafeMath for uint256;
 	mapping(address => mapping(address => uint256)) private _lockUpedDevValue;
 	function set(address propertyAddress, uint256 value) public {
-		_lockUpedDevValue[msg.sender][propertyAddress] = _lockUpedDevValue[msg.sender][propertyAddress] + value;
+		_lockUpedDevValue[msg.sender][propertyAddress] =
+			_lockUpedDevValue[msg.sender][propertyAddress] +
+			value;
 	}
 
-	function hasTokenByProperty(address propertyAddress) public view returns (bool){
+	function hasTokenByProperty(address propertyAddress)
+		public
+		view
+		returns (bool)
+	{
 		return _lockUpedDevValue[msg.sender][propertyAddress] != 0;
 	}
 }
@@ -64,7 +67,7 @@ contract CanceledLockUpFlg {
 	function setCancelFlg(address propertyAddress) public {
 		_canceled[msg.sender][propertyAddress] = true;
 	}
-	function isCanceled(address propertyAddress) public view returns (bool){
+	function isCanceled(address propertyAddress) public view returns (bool) {
 		return _canceled[msg.sender][propertyAddress];
 	}
 }
--- contracts/PolicyFactory.sol
+++ contracts/PolicyFactory.sol
@@ -3,13 +3,9 @@ pragma solidity ^0.5.0;
 import "./Policy.sol";
 
 contract PolicyFactory is UseState {
-
 	event Create(address indexed _from, address _property);
 
-	function createPolicy()
-		public
-		returns (address)
-	{
+	function createPolicy() public returns (address) {
 		Policy policy = new Policy(msg.sender);
 		addProperty(address(policy));
 		emit Create(msg.sender, address(policy));
