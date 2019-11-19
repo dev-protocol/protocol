7eccbb7c44317d24e268a6da87b2c05b68a0f23c
Akira-Taniguchi <einstein19821108>
2019-11-19 16:52:55 +0900

    delete unnecessary test

--- test/state.ts
+++ test/state.ts
@@ -1,8 +1,7 @@
 /* eslint-disable no-unused-expressions */
 
-contract('State', ([deployer, u1, u2, u3]) => {
+contract('State', ([deployer, u1, u2]) => {
 	const stateContract = artifacts.require('State')
-	const propertyContract = artifacts.require('property/Property')
 
 	describe('Roles; addMarket', () => {
 		it('Set Market Factory', async () => {
@@ -16,22 +15,6 @@ contract('State', ([deployer, u1, u2, u3]) => {
 			const contract = await stateContract.new({from: deployer})
 			const results = await contract
 				.setMarketFactory(u1, {from: u2})
-				.catch((err: Error) => err)
-			expect(results).to.instanceOf(Error)
-		})
-
-		it('Add market', async () => {
-			const contract = await stateContract.new({from: deployer})
-			await contract.setMarketFactory(u1, {from: deployer})
-			await contract.addMarket(u2, {from: u1})
-			const results = await contract.containsMarket(u2, {from: u1})
-			expect(results).to.be.equal(true)
-		})
-
-		it('Should fail to add Market when sent from the non-Market Factory account', async () => {
-			const contract = await stateContract.new({from: deployer})
-			const results = await contract
-				.addMarket(u1, {from: deployer})
 				.catch((err: Error) => err)
 			expect(results).to.instanceOf(Error)
 		})
@@ -61,78 +44,6 @@ contract('State', ([deployer, u1, u2, u3]) => {
 				.setToken(u1, {from: u2})
 				.catch((err: Error) => err)
 			expect(results).to.instanceOf(Error)
-		})
-	})
-
-	describe('Property token; addProperty', () => {
-		it('Add Property Contract token address', async () => {
-			const contract = await stateContract.new({from: deployer})
-			// Use u1 address as dummy PropertyFactory's address
-			await contract.setPropertyFactory(u1, {
-				from: deployer
-			})
-			const property = await propertyContract.new(
-				deployer,
-				'pkg_token',
-				'PKG',
-				18,
-				10000,
-				{
-					from: deployer
-				}
-			)
-			const results = await contract.addProperty(property.address, {
-				from: u1
-			})
-			expect(results).to.be.ok
-		})
-
-		it('Should fail to add Property Contract token address when sent from the non-Property Factory Contract', async () => {
-			const property = await propertyContract.new(
-				deployer,
-				'pkg_token',
-				'PKG',
-				18,
-				10000,
-				{
-					from: deployer
-				}
-			)
-			const contract = await stateContract.new({from: deployer})
-			await contract.setPropertyFactory(u1, {from: deployer})
-			const results = await contract
-				.addProperty(property.address, {from: property.address})
-				.catch((err: Error) => err)
-			expect(results).to.instanceOf(Error)
-		})
-	})
-
-	describe('Property token; isProperty', () => {
-		it('Verifying the passed address is a Property Contract address', async () => {
-			const address = '0x111122223333444455556666777788889999aAaa'
-			const contract = await stateContract.new({from: deployer})
-			await contract.setPropertyFactory(u1, {
-				from: deployer
-			})
-			await contract.addProperty(address, {
-				from: u1
-			})
-			const results = await contract.isProperty(address)
-			expect(results).to.be.equal(true)
-		})
-
-		it('Should fail to verify the passed address is a Property Contract address when not exists Property Contract', async () => {
-			const contract = await stateContract.new({from: deployer})
-			await contract.setPropertyFactory(u1, {
-				from: deployer
-			})
-			await contract.addProperty('0x40da26927c9d53106c0ca47608a4fdadf1ab6d29', {
-				from: u1
-			})
-			const results = await contract
-				.isProperty('0x111122223333444455556666777788889999aAaa')
-				.catch((err: Error) => err)
-			expect(results).to.be.equal(false)
 		})
 	})
 
@@ -180,53 +91,6 @@ contract('State', ([deployer, u1, u2, u3]) => {
 			expect(allocatorAddress).to.be.equal(
 				'0x0000000000000000000000000000000000000000'
 			)
-		})
-	})
-
-	describe('Metrics Contract; addMetrics', () => {
-		it('Add Metrics Contract token address', async () => {
-			const contract = await stateContract.new({from: deployer})
-			await contract.setMarketFactory(u1, {from: deployer})
-			await contract.addMarket(u2, {from: u1})
-			await contract.addMetrics(u3, {from: u2})
-			const result = await contract.isMetrics(u3, {from: deployer})
-			expect(result).to.be.equal(true)
-		})
-
-		it('Increaments totalIssuedMetrics', async () => {
-			const contract = await stateContract.new({from: deployer})
-			await contract.setMarketFactory(u1, {from: deployer})
-			await contract.addMarket(u2, {from: u1})
-			await contract.addMetrics(u3, {from: u2})
-			await contract.isMetrics(u3, {from: deployer})
-			const result = await contract.totalIssuedMetrics({from: deployer})
-			expect(result.toNumber()).to.be.equal(1)
-		})
-
-		it('Should fail to add Metrics Contract address when sent from the non-Market Contract', async () => {
-			const contract = await stateContract.new({from: deployer})
-			await contract.setMarketFactory(u1, {from: deployer})
-			await contract.addMarket(u2, {from: u1})
-			const result = await contract
-				.addMetrics(u3, {from: deployer})
-				.catch((err: Error) => err)
-			expect(result).to.instanceOf(Error)
-		})
-	})
-
-	describe('Metrics Contract; isMetrics', () => {
-		it('Verifying the passed address is a Metrics Contract address', async () => {
-			const contract = await stateContract.new({from: deployer})
-			await contract.setMarketFactory(u1, {from: deployer})
-			await contract.addMarket(u2, {from: u1})
-
-			const beforeResult = await contract.isMetrics(u3, {from: deployer})
-			expect(beforeResult).to.be.equal(false)
-
-			await contract.addMetrics(u3, {from: u2})
-
-			const afterResult = await contract.isMetrics(u3, {from: deployer})
-			expect(afterResult).to.be.equal(true)
 		})
 	})
 })
