0bcb0e4c1b2284342e2debb46f26ad67cd23aebf
Akira-Taniguchi <einstein19821108>
2019-11-20 22:23:20 +0900

    use policy setting in caluculation

--- contracts/Allocator.sol
+++ contracts/Allocator.sol
@@ -8,7 +8,9 @@ import "./libs/Withdrawable.sol";
 import "./market/Market.sol";
 import "./metrics/Metrics.sol";
 import "./metrics/MetricsGroup.sol";
+import "./policy/PolicyFactory.sol";
 import "./LastAllocationTime.sol";
+import "./Lockup.sol";
 
 contract Allocator is Killable, Ownable, UsingConfig, Withdrawable {
 	using SafeMath for uint256;
@@ -18,8 +20,6 @@ contract Allocator is Killable, Ownable, UsingConfig, Withdrawable {
 	uint256 public lastTotalAllocationValuePerBlock;
 
 	mapping(address => bool) pendingIncrements;
-	// TODO not set
-	uint256 public mintPerBlock;
 	LastAllocationTime private lastAllocationTime;
 
 	constructor(address _config) public UsingConfig(_config) {
@@ -61,24 +61,26 @@ contract Allocator is Killable, Ownable, UsingConfig, Withdrawable {
 			pendingIncrements[_metrics] == true,
 			"Not asking for an indicator"
 		);
-		address property = metrics.property();
-		uint256 share = market.issuedMetrics() /
-			MetricsGroup(config().metricsGroup()).totalIssuedMetrics();
+		Policy policy = Policy(config().policy());
+		Lockup lockup = Lockup(config().lockup());
+		uint256 totalIssuedMetrics = MetricsGroup(config().metricsGroup()).totalIssuedMetrics();
+		uint256 share = market.issuedMetrics() / totalIssuedMetrics;
 		uint256 period = block.number -
 			lastAllocationBlockEachMetrics[_metrics];
-		uint256 allocationPerBlock = _value / period;
+		uint256 lockupValue = lockup.getTokenValue(msg.sender, metrics.property());
+		uint256 allocationPerBlock = policy.assetValue(lockupValue, _value) / period;
 		uint256 nextTotalAllocationValuePerBlock = lastTotalAllocationValuePerBlock -
 			lastAllocationValueEachMetrics[_metrics] +
 			allocationPerBlock;
 		uint256 allocation = allocationPerBlock /
 			nextTotalAllocationValuePerBlock *
-			mintPerBlock *
+			policy.rewards(lockupValue, totalIssuedMetrics) *
 			share *
 			period;
 		lastAllocationBlockEachMetrics[_metrics] = block.number;
 		lastAllocationValueEachMetrics[_metrics] = allocationPerBlock;
 		lastTotalAllocationValuePerBlock = nextTotalAllocationValuePerBlock;
-		increment(property, allocation);
+		increment(metrics.property(), allocation);
 		delete pendingIncrements[_metrics];
 	}
 }
