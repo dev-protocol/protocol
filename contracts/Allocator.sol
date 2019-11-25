2d5afddb1265095eae29ddb2d45ac6e7dff4504e
aggre <hiroyuki.aggre@gmail.com>
2019-11-25 17:31:31 +0900

    implement calculation of the number of allocation and remove the unnecessaey test that became unused methods.

--- contracts/Allocator.sol
+++ contracts/Allocator.sol
@@ -5,6 +5,7 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 import "./libs/Killable.sol";
 import "./libs/Withdrawable.sol";
+import "./libs/Decimals.sol";
 import "./market/Market.sol";
 import "./metrics/Metrics.sol";
 import "./metrics/MetricsGroup.sol";
@@ -13,10 +14,12 @@ import "./policy/PolicyFactory.sol";
 
 contract Allocator is Killable, Ownable, UsingConfig, Withdrawable {
 	using SafeMath for uint256;
+	using Decimals for uint256;
 
 	mapping(address => uint256) lastAllocationBlockEachMetrics;
-	mapping(address => uint256) lastAllocationValueEachMetrics;
-	uint256 public lastTotalAllocationValuePerBlock;
+	mapping(address => uint256) lastAssetValueEachMetrics;
+	mapping(address => uint256) lastAssetValueEachMarketPerBlock;
+	uint64 public basis = 1000000000000000000;
 
 	mapping(address => bool) pendingIncrements;
 	// TODO not set
@@ -65,6 +68,20 @@ contract Allocator is Killable, Ownable, UsingConfig, Withdrawable {
 		counter.resetVoteCountByProperty(property);
 	}
 
+	function allocation(
+		uint256 _blocks,
+		uint256 _mint,
+		uint256 _value,
+		uint256 _marketValue,
+		uint256 _assets,
+		uint256 _totalAssets
+	) public pure returns (uint256) {
+		(uint256 aShare, uint256 aBasis) = _assets.outOf(_totalAssets);
+		(uint256 vShare, uint256 vBasis) = _value.outOf(_marketValue);
+		uint256 mint = _mint.mul(_blocks);
+		return mint.mul(aShare).mul(vShare) / aBasis / vBasis;
+	}
+
 	function calculatedCallback(address _metrics, uint256 _value) public {
 		Metrics metrics = Metrics(_metrics);
 		Market market = Market(metrics.market());
@@ -77,29 +94,28 @@ contract Allocator is Killable, Ownable, UsingConfig, Withdrawable {
 			"Not asking for an indicator"
 		);
 		Policy policy = Policy(config().policy());
-		Lockup lockup = Lockup(config().lockup());
-		uint256 totalIssuedMetrics = MetricsGroup(config().metricsGroup())
+		uint256 totalAssets = MetricsGroup(config().metricsGroup())
 			.totalIssuedMetrics();
-		uint256 share = market.issuedMetrics() / totalIssuedMetrics;
-		uint256 period = block.number -
-			lastAllocationBlockEachMetrics[_metrics];
-		uint256 lockupValue = lockup.getTokenValueByProperty(
+		uint256 lockupValue = Lockup(config().lockup()).getTokenValueByProperty(
 			metrics.property()
 		);
-		uint256 allocationPerBlock = policy.assetValue(lockupValue, _value) /
-			period;
-		uint256 nextTotalAllocationValuePerBlock = lastTotalAllocationValuePerBlock -
-			lastAllocationValueEachMetrics[_metrics] +
-			allocationPerBlock;
-		uint256 allocation = allocationPerBlock /
-			nextTotalAllocationValuePerBlock *
-			policy.rewards(lockupValue, totalIssuedMetrics) *
-			share *
-			period;
+
+		uint256 blocks = block.number -
+			lastAllocationBlockEachMetrics[_metrics];
+		uint256 mint = policy.rewards(lockupValue, totalAssets);
+		uint256 value = policy.assetValue(lockupValue, _value) * basis / blocks;
+		uint256 marketValue = lastAssetValueEachMarketPerBlock[metrics
+				.market()] -
+			lastAssetValueEachMetrics[_metrics] +
+			value;
+		uint256 assets = market.issuedMetrics();
 		lastAllocationBlockEachMetrics[_metrics] = block.number;
-		lastAllocationValueEachMetrics[_metrics] = allocationPerBlock;
-		lastTotalAllocationValuePerBlock = nextTotalAllocationValuePerBlock;
-		increment(metrics.property(), allocation);
+		lastAssetValueEachMetrics[_metrics] = value;
+		lastAssetValueEachMarketPerBlock[metrics.market()] = marketValue;
+		increment(
+			metrics.property(),
+			allocation(blocks, mint, value, marketValue, assets, totalAssets)
+		);
 		delete pendingIncrements[_metrics];
 	}
 }
