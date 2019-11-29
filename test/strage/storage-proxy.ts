c79d1afa1f3baf79630c408f37b0ce5d9dd2f8c9
Akira-Taniguchi <einstein19821108>
2019-11-29 21:23:45 +0900

    update commit error

--- test/strage/storage-proxy.ts
+++ test/strage/storage-proxy.ts
@@ -7,8 +7,10 @@ contract('StorageProxyTest', ([deployer]) => {
 		})
 		it('New storage can be created', async () => {
 			const result = await storageProxy.getStorage(
+				// eslint-disable-next-line no-undef
 				web3.utils.fromAscii('20160528')
 			)
+			console.log(result)
 		})
 		it('we can get existing storage', async () => {})
 	})
