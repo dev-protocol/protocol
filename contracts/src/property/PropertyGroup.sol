0c85f59d581ba663f452ab56481859a571038095
Akira-Taniguchi <einstein19821108>
2019-12-05 10:12:05 +0900

    update by lint

--- contracts/src/property/PropertyGroup.sol
+++ contracts/src/property/PropertyGroup.sol
@@ -1,23 +1,18 @@
 pragma solidity ^0.5.0;
 
-
 import "../common/modifier/UsingModifier.sol";
 import "../common/storage/UsingStorage.sol";
 
 contract PropertyGroup is UsingModifier, UsingStorage {
-
 	// solium-disable-next-line no-empty-blocks
-	constructor(address _config)
-		public
-		UsingModifier(_config)
-	{}
+	constructor(address _config) public UsingModifier(_config) {}
 
 	function addProperty(address _property) public onlyPropertyFactory {
 		require(_property != address(0), "property is an invalid address");
 		eternalStorage().setBool(getKey(_property), true);
 	}
 
-	function getKey(address _property) private pure returns (bytes32){
+	function getKey(address _property) private pure returns (bytes32) {
 		return keccak256(abi.encodePacked("_propertyGroup", _property));
 	}
 
