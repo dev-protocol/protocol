db5cad7d2a23f4b4e214aece1556483c2f8a4f6a
Akira-Taniguchi <einstein19821108>
2019-12-07 06:44:18 +0900

    add other contract

--- migrations/2_prepare.ts
+++ migrations/2_prepare.ts
@@ -1,11 +1,36 @@
 const handler = function(deployer) {
 	const {address} = artifacts.require('AddressConfig')
 
-	deployer.deploy(artifacts.require('Lockup'), address)
+	// Allocator
 	deployer.deploy(artifacts.require('Allocator'), address)
+	deployer.deploy(artifacts.require('Allocation'), address)
+	deployer.deploy(artifacts.require('LastWithdrawalPrice'), address)
+	deployer.deploy(artifacts.require('PendingWithdrawal'), address)
+
+	// lockup
+	deployer.deploy(artifacts.require('Lockup'), address)
+	deployer.deploy(artifacts.require('LockupPropertyValue'), address)
+	deployer.deploy(artifacts.require('LockupValue'), address)
+	deployer.deploy(artifacts.require('LockupWithdrawalStatus'), address)
+
+	// market
 	deployer.deploy(artifacts.require('MarketFactory'), address)
-	deployer.deploy(artifacts.require('PropertyFactory'), address)
+	deployer.deploy(artifacts.require('MarketGroup'), address)
+
+	//metrics
+	deployer.deploy(artifacts.require('MetricsGroup'), address)
+
+	// policy
 	deployer.deploy(artifacts.require('PolicyFactory'), address)
+	deployer.deploy(artifacts.require('PolicyGroup'))
+
+	// property
+	deployer.deploy(artifacts.require('PropertyFactory'), address)
+	deployer.deploy(artifacts.require('PropertyGroup'), address)
+
+	// vote
+	deployer.deploy(artifacts.require('VoteCounter'), address)
+	deployer.deploy(artifacts.require('VoteTimes'), address)
 } as Truffle.Migration
 
 export = handler
