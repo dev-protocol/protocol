b07bcd44c4a657e9f902839effeb359924ff1580
aggre <hiroyuki.aggre@gmail.com>
2019-12-11 16:20:26 +0900

    more simply setting envs

--- cli/index.ts
+++ cli/index.ts
@@ -1,20 +1,18 @@
 #!/usr/bin/env node
 import {exec} from 'shelljs'
 import * as program from 'commander'
-import {writeFileSync} from 'fs'
 
 program
 	.command('mock')
 	.option('-h, --host', 'Network host')
 	.option('-p, --port', 'Network port')
 	.action((_, [host = '127.0.0.1', port = 7545] = []) => {
-		writeFileSync(
-			'.env',
-			`ETHEREUM_MOCK_HOST=${host as string}
-			ETHEREUM_MOCK_PORT=${port as number}`.replace(/\t/g, '')
-		)
-		exec('npx truffle compile')
-		exec('npx truffle migrate --reset --network mock')
+		const env = {
+			ETHEREUM_MOCK_HOST: host,
+			ETHEREUM_MOCK_PORT: port
+		}
+		exec('npx truffle compile', {env})
+		exec('npx truffle migrate --reset --network mock', {env})
 	})
 
 program.parse(process.argv)
