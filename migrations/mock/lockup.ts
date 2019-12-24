cf6b35f881caabd1aa5cd72c8a84da46442438b3
Akira-Taniguchi <einstein19821108>
2019-12-24 17:19:05 +0900

    update by lint

--- migrations/mock/lockup.ts
+++ migrations/mock/lockup.ts
@@ -1,4 +1,4 @@
-// import {DummyDEVInstance} from '../../types/truffle-contracts'
+// Import {DummyDEVInstance} from '../../types/truffle-contracts'
 import {AddressInfo} from './interface'
 import {getDummyDevInstance} from './token'
 import BigNumber from 'bignumber.js'
@@ -13,7 +13,14 @@ export async function lockup(
 	const dummyDev = await getDummyDevInstance(artifacts)
 	const tmp = await dummyDev.decimals()
 	const decimals = 10 ** tmp.toNumber()
-	await lockup.lockup(addressInfo[0].property!, new BigNumber(20000 * decimals), {from:addressInfo[0].account})
-	const value = await lockup.getValue(addressInfo[0].property!, addressInfo[0].account)
+	await lockup.lockup(
+		addressInfo[0].property!,
+		new BigNumber(20000 * decimals),
+		{from: addressInfo[0].account}
+	)
+	const value = await lockup.getValue(
+		addressInfo[0].property!,
+		addressInfo[0].account
+	)
 	console.log(value)
 }
