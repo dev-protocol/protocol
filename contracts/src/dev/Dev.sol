14a05b0d3183cfd1a6575403e790430ec4857c18
aggre <hiroyuki.aggre@gmail.com>
2019-12-26 09:28:44 +0900

    WIP: add new dev tokens

---
+++ contracts/src/dev/Dev.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.0;
+
+import {ERC20Detailed} from "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import {ERC20Mintable} from "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import {ERC20Burnable} from "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
+import {UsingConfig} from "contracts/src/common/config/UsingConfig.sol";
+import {Lockup} from "contracts/src/lockup/Lockup.sol";
+
+contract Dev is ERC20Detailed, ERC20Mintable, ERC20Burnable, UsingConfig {
+	constructor(address _config)
+		public
+		ERC20Detailed("Dev", "DEV", 18)
+		UsingConfig(_config)
+	{}
+
+	function deposit(address _to, uint256 _amount) public returns (bool) {
+		transfer(_to, _amount);
+		Lockup(config().lockup()).lockup(_to, _amount);
+		return true;
+	}
+}
---
+++ contracts/src/dev/DevMigration.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.5.0;
+
+import {ERC20Mintable} from "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import {ERC20Burnable} from "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
+
+contract DevMigration {
+	address public legacy;
+	address public next;
+	constructor(address _legacy, address _next) public {
+		legacy = _legacy;
+		next = _next;
+	}
+
+	function migration() public returns (bool) {
+		ERC20Burnable _legacy = ERC20Burnable(legacy);
+		ERC20Mintable _next = ERC20Mintable(next);
+		uint256 balance = _legacy.balanceOf(msg.sender);
+		_legacy.burn(balance);
+		_next.mint(msg.sender, balance);
+		return true;
+	}
+}
