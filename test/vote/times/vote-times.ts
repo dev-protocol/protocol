3137302b48d588c39970fe551f01649791becd4a
Akira-Taniguchi <einstein19821108>
2019-12-26 21:33:27 +0900

    add test class

--- test/vote/times/vote-times.ts
+++ test/vote/times/vote-times.ts
@@ -1,3 +1,6 @@
+import {DevProtpcolInstance} from './../../lib/instance'
+import {VoteTimesStorageInstance} from '../../../types/truffle-contracts'
+
 contract(
 	'VoteTimesTest',
 	([
@@ -8,92 +11,63 @@ contract(
 		dummyProperty,
 		voteCounter
 	]) => {
-		const voteTimesTestContract = artifacts.require('VoteTimes')
-		const addressConfigContract = artifacts.require('AddressConfig')
-		const propertyGroupContract = artifacts.require('PropertyGroup')
-		const voteTimesStorageContract = artifacts.require('VoteTimesStorage')
+		const dev = new DevProtpcolInstance(deployer)
 		describe('VoteTimes; addVoteTimes, addVoteTimesByProperty, resetVoteTimesByProperty', () => {
-			let voteTimes: any
-			let voteTimesStorage: any
-			let addressConfig: any
-			let propertyGroup: any
 			before(async () => {
-				addressConfig = await addressConfigContract.new({from: deployer})
-				voteTimes = await voteTimesTestContract.new(addressConfig.address, {
+				await dev.generateAddressConfig()
+				await dev.generateVoteTimes()
+				await dev.generateVoteTimesStorage()
+				await dev.generatePropertyGroup()
+				await dev.addressConfig.setMarketFactory(marketFactory, {
 					from: deployer
 				})
-				await addressConfig.setVoteTimes(voteTimes.address, {
+				await dev.addressConfig.setPropertyFactory(propertyFactory, {
 					from: deployer
 				})
-				await addressConfig.setMarketFactory(marketFactory, {
-					from: deployer
-				})
-				await addressConfig.setPropertyFactory(propertyFactory, {
-					from: deployer
-				})
-				await addressConfig.setVoteCounter(voteCounter, {
-					from: deployer
-				})
-				propertyGroup = await propertyGroupContract.new(addressConfig.address, {
-					from: deployer
-				})
-				await addressConfig.setPropertyGroup(propertyGroup.address, {
-					from: deployer
-				})
-				await propertyGroup.createStorage()
-				await propertyGroup.addGroup(property, {from: propertyFactory})
-				voteTimesStorage = await voteTimesStorageContract.new(
-					addressConfig.address,
-					{
-						from: deployer
-					}
-				)
-				await voteTimesStorage.createStorage()
-				await addressConfig.setVoteTimesStorage(voteTimesStorage.address, {
-					from: deployer
-				})
-				await voteTimes.addVoteTime({from: marketFactory})
-				await voteTimes.addVoteTime({from: marketFactory})
-				await voteTimes.addVoteTimesByProperty(property, {
+				await dev.addressConfig.setVoteCounter(voteCounter, {from: deployer})
+				await dev.propertyGroup.addGroup(property, {from: propertyFactory})
+				await dev.voteTimes.addVoteTime({from: marketFactory})
+				await dev.voteTimes.addVoteTime({from: marketFactory})
+				await dev.voteTimes.addVoteTimesByProperty(property, {
 					from: voteCounter
 				})
 			})
 			it('If the vote was held twice, but the vote was held only once, the number of abstentions will be 1.', async () => {
-				const result = await voteTimes.getAbstentionTimes(property)
+				const result = await dev.voteTimes.getAbstentionTimes(property)
 				expect(result.toNumber()).to.be.equal(1)
 			})
 			it('When reset, the number of abstentions becomes 0.', async () => {
-				await voteTimes.resetVoteTimesByProperty(property, {
+				await dev.voteTimes.resetVoteTimesByProperty(property, {
 					from: propertyFactory
 				})
-				const result = await voteTimes.getAbstentionTimes(property)
+				const result = await dev.voteTimes.getAbstentionTimes(property)
 				expect(result.toNumber()).to.be.equal(0)
 			})
 			it('If the number of votes is 0, the number of votes cast is the number of abstentions.', async () => {
-				const result = await voteTimes.getAbstentionTimes(dummyProperty)
+				const result = await dev.voteTimes.getAbstentionTimes(dummyProperty)
 				expect(result.toNumber()).to.be.equal(2)
 			})
 			it('Storage information can be taken over.', async () => {
-				const storageAddress = await voteTimesStorage.getStorageAddress({
+				const storageAddress = await dev.voteTimesStorage.getStorageAddress({
 					from: deployer
 				})
-				const newVoteTimesStorage = await voteTimesStorageContract.new(
-					addressConfig.address,
+				const newVoteTimesStorage = await dev.generateInstance<
+					VoteTimesStorageInstance
+				>('VoteTimesStorage')
+				await newVoteTimesStorage.setStorage(storageAddress, {
+					from: deployer
+				})
+				await dev.voteTimesStorage.changeOwner(newVoteTimesStorage.address, {
+					from: deployer
+				})
+				await dev.addressConfig.setVoteTimesStorage(
+					newVoteTimesStorage.address,
 					{
 						from: deployer
 					}
 				)
-				await newVoteTimesStorage.setStorage(storageAddress, {
-					from: deployer
-				})
-				await voteTimesStorage.changeOwner(newVoteTimesStorage.address, {
-					from: deployer
-				})
-				await addressConfig.setVoteTimesStorage(newVoteTimesStorage.address, {
-					from: deployer
-				})
-				await voteTimes.addVoteTime({from: marketFactory})
-				const result = await voteTimes.getAbstentionTimes(property)
+				await dev.voteTimes.addVoteTime({from: marketFactory})
+				const result = await dev.voteTimes.getAbstentionTimes(property)
 				expect(result.toNumber()).to.be.equal(1)
 			})
 		})
