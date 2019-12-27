ff7266b49dfae829770e27981e7a9ad1c7453b3f
aggre <hiroyuki.aggre@gmail.com>
2019-12-27 12:13:53 +0900

    add burn method for authentication fee

--- contracts/src/dev/Dev.sol
+++ contracts/src/dev/Dev.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.5.0;
 import {ERC20Detailed} from "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import {ERC20Mintable} from "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 import {ERC20Burnable} from "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
+import {AddressValidator} from "../common/validate/AddressValidator.sol";
 import {UsingConfig} from "../common/config/UsingConfig.sol";
 import {Lockup} from "../lockup/Lockup.sol";
 
@@ -16,6 +17,12 @@ contract Dev is ERC20Detailed, ERC20Mintable, ERC20Burnable, UsingConfig {
 	function deposit(address _to, uint256 _amount) public returns (bool) {
 		transfer(_to, _amount);
 		Lockup(config().lockup()).lockup(_to, _amount);
+		return true;
+	}
+
+	function fee(address _from, uint256 _amount) public returns (bool) {
+		new AddressValidator().validateGroup(msg.sender, config().marketGroup());
+		_burn(_from, _amount);
 		return true;
 	}
 }
