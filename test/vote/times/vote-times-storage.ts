c1a95ef097694e84dfd20f3b170dceee2f503363
Akira-Taniguchi <einstein19821108>
2019-12-27 10:23:28 +0900

    add vote times storage test

--- test/vote/times/vote-times-storage.ts
+++ test/vote/times/vote-times-storage.ts
@@ -1,50 +1,50 @@
+import {DevProtpcolInstance} from './../../lib/instance'
+import {VoteTimesStorageInstance} from '../../../types/truffle-contracts'
+
 contract(
 	'VoteTimesStorageTest',
 	([deployer, voteTimes, property, property2]) => {
-		const voteTimesStorageContract = artifacts.require('VoteTimesStorage')
-		const addressConfigContract = artifacts.require('AddressConfig')
-		let voteTimesStorage: any
-		let addressConfig: any
+		const dev = new DevProtpcolInstance(deployer)
 		before(async () => {
-			addressConfig = await addressConfigContract.new({from: deployer})
-			voteTimesStorage = await voteTimesStorageContract.new(
-				addressConfig.address,
-				{
-					from: deployer
-				}
-			)
-			await voteTimesStorage.createStorage()
-			await addressConfig.setVoteTimes(voteTimes, {from: deployer})
+			await dev.generateAddressConfig()
+			await dev.generateVoteTimesStorage()
+			await dev.addressConfig.setVoteTimes(voteTimes, {from: deployer})
 		})
 		describe('VoteTimesStorage; getVoteTimes, setVoteTimes', () => {
 			it('Initial value is 0.', async () => {
-				const result = await voteTimesStorage.getVoteTimes({from: voteTimes})
+				const result = await dev.voteTimesStorage.getVoteTimes({
+					from: voteTimes
+				})
 				expect(result.toNumber()).to.be.equal(0)
 			})
 			it('The set value can be taken as it is.', async () => {
-				await voteTimesStorage.setVoteTimes(3, {from: voteTimes})
-				const result = await voteTimesStorage.getVoteTimes({from: voteTimes})
+				await dev.voteTimesStorage.setVoteTimes(3, {from: voteTimes})
+				const result = await dev.voteTimesStorage.getVoteTimes({
+					from: voteTimes
+				})
 				expect(result.toNumber()).to.be.equal(3)
 			})
 		})
 		describe('VoteTimesStorage; getVoteTimesByProperty, setVoteTimesByProperty', () => {
 			it('Initial value is 0.', async () => {
-				const result = await voteTimesStorage.getVoteTimesByProperty(property, {
-					from: voteTimes
-				})
+				const result = await dev.voteTimesStorage.getVoteTimesByProperty(
+					property,
+					{from: voteTimes}
+				)
 				expect(result.toNumber()).to.be.equal(0)
 			})
 			it('The set value can be taken as it is.', async () => {
-				await voteTimesStorage.setVoteTimesByProperty(property, 3, {
+				await dev.voteTimesStorage.setVoteTimesByProperty(property, 3, {
 					from: voteTimes
 				})
-				const result = await voteTimesStorage.getVoteTimesByProperty(property, {
-					from: voteTimes
-				})
+				const result = await dev.voteTimesStorage.getVoteTimesByProperty(
+					property,
+					{from: voteTimes}
+				)
 				expect(result.toNumber()).to.be.equal(3)
 			})
 			it('If not set, initial value can be taken.', async () => {
-				const result = await voteTimesStorage.getVoteTimesByProperty(
+				const result = await dev.voteTimesStorage.getVoteTimesByProperty(
 					property2,
 					{from: voteTimes}
 				)
@@ -53,15 +53,12 @@ contract(
 		})
 		describe('VoteTimesStorage; getStorageAddress, setStorage, changeOwner', () => {
 			it('Value is inherited.', async () => {
-				const newVoteTimesStorage = await voteTimesStorageContract.new(
-					addressConfig.address,
-					{
-						from: deployer
-					}
-				)
-				const storageAddress = await voteTimesStorage.getStorageAddress()
+				const newVoteTimesStorage = await dev.generateInstance<
+					VoteTimesStorageInstance
+				>('VoteTimesStorage')
+				const storageAddress = await dev.voteTimesStorage.getStorageAddress()
 				await newVoteTimesStorage.setStorage(storageAddress, {from: deployer})
-				await voteTimesStorage.changeOwner(newVoteTimesStorage.address, {
+				await dev.voteTimesStorage.changeOwner(newVoteTimesStorage.address, {
 					from: deployer
 				})
 				const result = await newVoteTimesStorage.getVoteTimesByProperty(
