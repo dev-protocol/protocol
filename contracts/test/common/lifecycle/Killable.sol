8576fd8ddd05bda579319746523e9a184b02bd94
Akira-Taniguchi <einstein19821108>
2020-01-06 22:44:19 +0900

    add test(kill able)

---
+++ contracts/test/common/lifecycle/Killable.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.0;
+
+import {Killable} from "contracts/src/common/lifecycle/Killable.sol";
+
+// solium-disable-next-line no-empty-blocks
+contract KillableTest is Killable {
+	function getValue()external pure returns (uint256){
+		return 1;
+	}
+}
---
+++ test/common/lifecycle/killable.ts
@@ -0,0 +1,24 @@
+import {validateErrorMessage} from '../../test-lib/error-utils'
+
+contract('KillableTest', ([deployer, user]) => {
+	const killableTestContract = artifacts.require('KillableTest')
+	describe('Killable; kill', () => {
+		it('コントラクトがkillされて関数が実行d系なくなる', async () => {
+			const killableTest = await killableTestContract.new({
+				from: deployer
+			})
+			const value = await killableTest.getValue()
+			expect(value.toNumber()).to.be.equal(1)
+			await killableTest.kill({from: deployer})
+			const result = await killableTest.getValue().catch((err: Error) => err)
+			expect(result.message).to.be.equal('Returned values aren\'t valid, did it run Out of Gas?')
+		})
+		it('デプロイしたアカウントしかkillできない', async () => {
+			const killableTest = await killableTestContract.new({
+				from: deployer
+			})
+			const result = await killableTest.kill({from: user}).catch((err: Error) => err)
+			validateErrorMessage(result as Error, 'only owner method')
+		})
+	})
+})
