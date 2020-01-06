425107097c64e224c987c43394fdbbd237f0a827
Akira-Taniguchi <einstein19821108>
2020-01-06 17:19:07 +0900

    update by lint

--- test/metrics/metrics.ts
+++ test/metrics/metrics.ts
@@ -2,7 +2,9 @@ contract('metricsTest', ([metricsFactory, market, property]) => {
 	const metricsContract = artifacts.require('Metrics')
 	describe('Metrics; constructor', () => {
 		it('The set address can be referenced.', async () => {
-			const metrics = await metricsContract.new(market, property, {from: metricsFactory})
+			const metrics = await metricsContract.new(market, property, {
+				from: metricsFactory
+			})
 			expect(await metrics.market()).to.be.equal(market)
 			expect(await metrics.property()).to.be.equal(property)
 		})
