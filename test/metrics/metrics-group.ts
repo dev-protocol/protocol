a1590cd67cf1640c2a25b74595e33d6bff749cf9
Akira-Taniguchi <einstein19821108>
2020-01-07 11:33:07 +0900

    add metrics test

--- test/metrics/metrics-group.ts
+++ test/metrics/metrics-group.ts
@@ -1,36 +1,49 @@
+import {DevProtocolInstance} from '../test-lib/instance'
+import {validateErrorMessage} from '../test-lib/error-utils'
+
 contract(
 	'MetricsGroupTest',
-	([deployer, metricsFactory, metrics, dummyMetrics]) => {
-		const addressConfigContract = artifacts.require('AddressConfig')
-		const metricsGroupContract = artifacts.require('MetricsGroup')
-		describe('MetricsGroup; isMetrics', () => {
-			// Let e XpectedMetorics Address: any
-			let metricsGroup: any
-			beforeEach(async () => {
-				const addressConfig = await addressConfigContract.new({
-					from: deployer
-				})
-				metricsGroup = await metricsGroupContract.new(addressConfig.address, {
-					from: deployer
-				})
-				await metricsGroup.createStorage()
-				await addressConfig.setMetricsGroup(metricsGroup.address, {
-					from: deployer
-				})
-				await addressConfig.setMetricsFactory(metricsFactory, {
-					from: deployer
-				})
-				metricsGroup.addGroup(metrics, {
+	([deployer, metricsFactory, metrics1, metrics2, dummyMetrics]) => {
+		const dev = new DevProtocolInstance(deployer)
+		before(async () => {
+			await dev.generateAddressConfig()
+			await dev.generateMetricsGroup()
+			await dev.addressConfig.setMetricsFactory(metricsFactory, {
+				from: deployer
+			})
+		})
+		describe('MetricsGroup; addGroup, isGroup', () => {
+			before(async () => {
+				await dev.metricsGroup.addGroup(metrics1, {
 					from: metricsFactory
 				})
 			})
 			it('When the metrics address is Specified', async () => {
-				const result = await metricsGroup.isGroup(metrics)
+				const result = await dev.metricsGroup.isGroup(metrics1)
 				expect(result).to.be.equal(true)
 			})
 			it('When the metrics address is not specified', async () => {
-				const result = await metricsGroup.isGroup(dummyMetrics)
+				const result = await dev.metricsGroup.isGroup(dummyMetrics)
 				expect(result).to.be.equal(false)
+			})
+			it('Existing metrics cannot be added', async () => {
+				const result = await dev.metricsGroup
+					.addGroup(metrics1, {
+						from: metricsFactory
+					})
+					.catch((err: Error) => err)
+				validateErrorMessage(result as Error, 'already enabled')
+			})
+		})
+		describe('MetricsGroup; totalIssuedMetrics', () => {
+			it('Count increases when metrics are added', async () => {
+				let result = await dev.metricsGroup.totalIssuedMetrics()
+				expect(result.toNumber()).to.be.equal(1)
+				await dev.metricsGroup.addGroup(metrics2, {
+					from: metricsFactory
+				})
+				result = await dev.metricsGroup.totalIssuedMetrics()
+				expect(result.toNumber()).to.be.equal(2)
 			})
 		})
 	}
