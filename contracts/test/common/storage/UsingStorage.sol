55f38045b63a6b6a8f8deb7bb0936d4b4e0c12fd
Akira-Taniguchi <einstein19821108>
2020-01-08 13:28:55 +0900

    add using storage test case

---
+++ contracts/test/common/storage/UsingStorage.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.0;
+
+import {UsingStorage} from "contracts/src/common/storage/UsingStorage.sol";
+
+contract UsingStorageTest is UsingStorage {
+	function setUInt(uint256 _value) external {
+		eternalStorage().setUint(getUintKey(), _value);
+	}
+
+	function getUInt() external view returns (uint256){
+		return eternalStorage().getUint(getUintKey());
+	}
+
+	function getUintKey()
+		private
+		pure
+		returns (bytes32)
+	{
+		return keccak256(abi.encodePacked("_uint"));
+	}
+}
