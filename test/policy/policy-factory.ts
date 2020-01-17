1ab58c3230e184e3cc4132e20b8bd16f717e9833
Akira-Taniguchi <einstein19821108>
2020-01-17 17:25:51 +0900

    add same test

--- test/policy/policy-factory.ts
+++ test/policy/policy-factory.ts
@@ -1,12 +1,17 @@
 import {IPolicyInstance, PolicyInstance} from '../../types/truffle-contracts'
 import {DevProtocolInstance, UserInstance} from '../test-lib/instance'
-import {getPolicyAddress, validateErrorMessage, mine} from '../test-lib/utils'
+import {
+	getPolicyAddress,
+	validateErrorMessage,
+	mine,
+	getPropertyAddress
+} from '../test-lib/utils'
 
 contract(
 	'PolicyFactory',
-	([deployer, user, property, propertyFactory, dummyProperty]) => {
+	([deployer, user1, user2, dummyProperty, propertyAuther]) => {
 		const dev = new DevProtocolInstance(deployer)
-		const userInstance = new UserInstance(dev, user)
+		const userInstance = new UserInstance(dev, user1)
 		let policy: IPolicyInstance
 		describe('PolicyFactory; createPolicy', () => {
 			beforeEach(async () => {
@@ -22,7 +27,7 @@ contract(
 			it('Returns the new Policy address when Policy implementation is passed.', async () => {
 				policy = await userInstance.getPolicy('PolicyTest1')
 				const result = await dev.policyFactory.create(policy.address, {
-					from: user
+					from: user1
 				})
 				const firstPolicyAddress = getPolicyAddress(result)
 				// eslint-disable-next-line no-undef
@@ -31,7 +36,7 @@ contract(
 			it('If the first Policy, the Policy becomes valid.', async () => {
 				policy = await userInstance.getPolicy('PolicyTest1')
 				const result = await dev.policyFactory.create(policy.address, {
-					from: user
+					from: user1
 				})
 				const firstPolicyAddress = getPolicyAddress(result)
 				const curentPolicyAddress = await dev.addressConfig.policy()
@@ -40,7 +45,7 @@ contract(
 			it('The first policy will be treated as voting completed.', async () => {
 				policy = await userInstance.getPolicy('PolicyTest1')
 				const result = await dev.policyFactory.create(policy.address, {
-					from: user
+					from: user1
 				})
 				const firstPolicyAddress = getPolicyAddress(result)
 				// eslint-disable-next-line @typescript-eslint/await-thenable
@@ -53,13 +58,13 @@ contract(
 			it('If other than the first Policy, the Policy is waiting for enable by the voting.', async () => {
 				policy = await userInstance.getPolicy('PolicyTest1')
 				await dev.policyFactory.create(policy.address, {
-					from: user
+					from: user1
 				})
 				const second = await userInstance.getPolicy('PolicyTest1')
 				const secondCreateResult = await dev.policyFactory.create(
 					second.address,
 					{
-						from: user
+						from: user1
 					}
 				)
 				const secondPolicyAddress = getPolicyAddress(secondCreateResult)
@@ -73,13 +78,13 @@ contract(
 			it('The maximum number of votes is incremented.', async () => {
 				policy = await userInstance.getPolicy('PolicyTest1')
 				await dev.policyFactory.create(policy.address, {
-					from: user
+					from: user1
 				})
 				let times = await dev.voteTimes.getAbstentionTimes(dummyProperty)
 				expect(times.toNumber()).to.be.equal(0)
 				const second = await userInstance.getPolicy('PolicyTest1')
 				await dev.policyFactory.create(second.address, {
-					from: user
+					from: user1
 				})
 				times = await dev.voteTimes.getAbstentionTimes(dummyProperty)
 				expect(times.toNumber()).to.be.equal(1)
@@ -88,6 +93,7 @@ contract(
 		describe('A new Policy; vote', () => {
 			const policyContract = artifacts.require('Policy')
 			let firstPolicyInstance: PolicyInstance
+			let createdPropertyAddress: string
 			beforeEach(async () => {
 				await dev.generateAddressConfig()
 				await dev.generatePolicyGroup()
@@ -96,20 +102,34 @@ contract(
 				await dev.generateVoteTimes()
 				await dev.generateVoteTimesStorage()
 				await dev.generateVoteCounter()
+				await dev.generateVoteCounterStorage()
 				await dev.generateMarketFactory()
 				await dev.generateMarketGroup()
 				await dev.generatePropertyGroup()
-				await dev.addressConfig.setPropertyFactory(propertyFactory, {
-					from: deployer
-				})
+				await dev.generatePropertyFactory()
+				await dev.generateLockup()
+				await dev.generateLockupStorage()
+				await dev.generateAllocator()
+				await dev.generateAllocatorStorage()
+				await dev.generateWithdraw()
+				await dev.generateWithdrawStorage()
+				await dev.generateDev()
+				await dev.dev.mint(user1, 100, {from: deployer})
+				await dev.dev.mint(user2, 100, {from: deployer})
 				policy = await userInstance.getPolicy('PolicyTest1')
-				const result = await dev.policyFactory.create(policy.address, {
-					from: user
-				})
-				const firstPolicyAddress = getPolicyAddress(result)
+				const policyCreateResult = await dev.policyFactory.create(
+					policy.address,
+					{from: user1}
+				)
+				const firstPolicyAddress = getPolicyAddress(policyCreateResult)
 				// eslint-disable-next-line @typescript-eslint/await-thenable
 				firstPolicyInstance = await policyContract.at(firstPolicyAddress)
-				await dev.propertyGroup.addGroup(property, {from: propertyFactory})
+				const propertyCreateResult = await dev.propertyFactory.create(
+					'test',
+					'TST',
+					propertyAuther
+				)
+				createdPropertyAddress = getPropertyAddress(propertyCreateResult)
 			})
 			it('Should fail when 1st args is not Property address.', async () => {
 				const result = await firstPolicyInstance
@@ -119,14 +139,14 @@ contract(
 			})
 			it('Should fail voting to the already enable Policy.', async () => {
 				const result = await firstPolicyInstance
-					.vote(property, true)
+					.vote(createdPropertyAddress, true)
 					.catch((err: Error) => err)
 				validateErrorMessage(result as Error, 'this policy is current')
 			})
 			it('Should fail to vote when after the voting period.', async () => {
 				const second = await userInstance.getPolicy('PolicyTest1')
 				const result = await dev.policyFactory.create(second.address, {
-					from: user
+					from: user1
 				})
 				const secondPolicyAddress = getPolicyAddress(result)
 				// eslint-disable-next-line @typescript-eslint/await-thenable
@@ -135,17 +155,15 @@ contract(
 				)
 				const count = await secondPolicyInstance.policyVotingBlocks()
 				await mine(count.toNumber())
-
 				const voteResult = await secondPolicyInstance
-					.vote(property, true)
+					.vote(createdPropertyAddress, true)
 					.catch((err: Error) => err)
 				validateErrorMessage(voteResult as Error, 'voting deadline is over')
 			})
 			it('Should fail to vote when the number of lockups and the market reward is 0.', async () => {
-				// ロックアップ数と市場報酬が0の場合、投票に失敗するはずです
 				const second = await userInstance.getPolicy('PolicyTest1')
 				const result = await dev.policyFactory.create(second.address, {
-					from: user
+					from: user1
 				})
 				const secondPolicyAddress = getPolicyAddress(result)
 				// eslint-disable-next-line @typescript-eslint/await-thenable
@@ -153,50 +171,128 @@ contract(
 					secondPolicyAddress
 				)
 				const voteResult = await secondPolicyInstance
-					.vote(property, true)
+					.vote(createdPropertyAddress, true)
 					.catch((err: Error) => err)
 				validateErrorMessage(voteResult as Error, 'vote count is 0')
 			})
 			it('Should fail to vote when already voted.', async () => {
-				// すでに投票したときに投票に失敗するはずです
+				const second = await userInstance.getPolicy('PolicyTest1')
+				const result = await dev.policyFactory.create(second.address, {
+					from: user1
+				})
+				const secondPolicyAddress = getPolicyAddress(result)
+				// eslint-disable-next-line @typescript-eslint/await-thenable
+				const secondPolicyInstance = await policyContract.at(
+					secondPolicyAddress
+				)
+				await dev.dev.deposit(createdPropertyAddress, 100, {from: user1})
+				await secondPolicyInstance.vote(createdPropertyAddress, true, {
+					from: user1
+				})
+				const voteResult = await secondPolicyInstance
+					.vote(createdPropertyAddress, true, {from: user1})
+					.catch((err: Error) => err)
+				validateErrorMessage(voteResult as Error, 'already vote')
 			})
 			it('The number of lock-ups for it Property and the accumulated Market reward will be added to the vote when a Property owner votes for.', async () => {
-				// プロパティのロックアップの数と累積されたマーケット報酬は、プロパティの所有者が投票したときに投票に追加されます。
+				const second = await userInstance.getPolicy('PolicyTest1')
+				const result = await dev.policyFactory.create(second.address, {
+					from: user1
+				})
+				const secondPolicyAddress = getPolicyAddress(result)
+				// eslint-disable-next-line @typescript-eslint/await-thenable
+				const secondPolicyInstance = await policyContract.at(
+					secondPolicyAddress
+				)
+				let agreeCount = await dev.voteCounter.getAgreeCount(
+					secondPolicyAddress
+				)
+				expect(agreeCount.toNumber()).to.be.equal(0)
+				await dev.dev.deposit(createdPropertyAddress, 100, {from: user1})
+				await dev.dev.deposit(createdPropertyAddress, 100, {from: user2})
+				await secondPolicyInstance.vote(createdPropertyAddress, true, {
+					from: propertyAuther
+				})
+				agreeCount = await dev.voteCounter.getAgreeCount(secondPolicyAddress)
+				expect(agreeCount.toNumber()).to.be.equal(200)
 			})
-			it('The number of votes VoteCounter is added when the Property owner voted.', async () => {
-				// 投票数VoteCounterは、所有者が投票したときに追加されます
+			it('The number of votes VoteTimes is added when the Property owner voted.', async () => {
+				let times = await dev.voteTimes.getAbstentionTimes(
+					createdPropertyAddress
+				)
+				expect(times.toNumber()).to.be.equal(0)
+				const second = await userInstance.getPolicy('PolicyTest1')
+				const result = await dev.policyFactory.create(second.address, {
+					from: user1
+				})
+				times = await dev.voteTimes.getAbstentionTimes(createdPropertyAddress)
+				expect(times.toNumber()).to.be.equal(1)
+				const secondPolicyAddress = getPolicyAddress(result)
+				// eslint-disable-next-line @typescript-eslint/await-thenable
+				const secondPolicyInstance = await policyContract.at(
+					secondPolicyAddress
+				)
+				await dev.dev.deposit(createdPropertyAddress, 100, {from: user1})
+				await secondPolicyInstance.vote(createdPropertyAddress, true, {
+					from: propertyAuther
+				})
+				times = await dev.voteTimes.getAbstentionTimes(createdPropertyAddress)
+				expect(times.toNumber()).to.be.equal(0)
 			})
-			it('VoteCounter votes will not be added when a vote by other than Property owner voted for.', async () => {
-				// 不動産所有者以外の投票が投票された場合、VoteCounter投票は追加されません。
+			it('VoteTimes votes will not be added when a vote by other than Property owner voted for.', async () => {
+				// 不動産所有者以外の投票が投票された場合、VoteTimes投票は追加されません。
+				let times = await dev.voteTimes.getAbstentionTimes(
+					createdPropertyAddress
+				)
+				console.log(1)
+				expect(times.toNumber()).to.be.equal(0)
+				console.log(2)
+				const second = await userInstance.getPolicy('PolicyTest1')
+				const result = await dev.policyFactory.create(second.address, {
+					from: user1
+				})
+				const secondPolicyAddress = getPolicyAddress(result)
+				// eslint-disable-next-line @typescript-eslint/await-thenable
+				const secondPolicyInstance = await policyContract.at(
+					secondPolicyAddress
+				)
+				await dev.dev.deposit(createdPropertyAddress, 100, {from: user1})
+				await secondPolicyInstance.vote(createdPropertyAddress, true, {
+					from: user1
+				})
+				times = await dev.voteTimes.getAbstentionTimes(createdPropertyAddress)
+				console.log(3)
+				expect(times.toNumber()).to.be.equal(0)
+				console.log(4)
 			})
 
-			it('The number of lock-ups for it Property and the accumulated Market reward will be added to the vote against when a Property owner votes against.', async () => {
-				// 不動産所有者が反対票を投じると、その資産のロックアップ数と累積市場報酬が反対票に追加されます
-			})
-			it('The number of votes VoteCounter is added when the Property owner votes against.', async () => {
-				// 所有者が反対票を投じると、投票数VoteCounterが追加されます。
-			})
-			it('VoteCounter votes will not be added when a vote by other than Property owner voted against.', async () => {
-				// 物件所有者以外の投票が反対票を投じた場合、VoteCounter投票は追加されません。
-			})
+			// It('The number of lock-ups for it Property and the accumulated Market reward will be added to the vote against when a Property owner votes against.', async () => {
+			// 	// 不動産所有者が反対票を投じると、その資産のロックアップ数と累積市場報酬が反対票に追加されます
+			// })
+			// it('The number of votes VoteCounter is added when the Property owner votes against.', async () => {
+			// 	// 所有者が反対票を投じると、投票数VoteCounterが追加されます。
+			// })
+			// it('VoteCounter votes will not be added when a vote by other than Property owner voted against.', async () => {
+			// 	// 物件所有者以外の投票が反対票を投じた場合、VoteCounter投票は追加されません。
+			// })
 
-			it('When an account of other than Property owner votes for, the number of lock-ups in the Property by its account will be added to the vote.', async () => {
-				// プロパティ所有者以外のアカウントが投票する場合、そのアカウントによるプロパティのロックアップの数が投票に追加されます。
-			})
+			// it('When an account of other than Property owner votes for, the number of lock-ups in the Property by its account will be added to the vote.', async () => {
+			// 	// プロパティ所有者以外のアカウントが投票する場合、そのアカウントによるプロパティのロックアップの数が投票に追加されます。
+			// })
 		})
-		describe('PolicyFactory; convergePolicy', () => {
-			it('Calling `convergePolicy` method when approved by Policy.policyApproval.', async () => {
-				// Policy.policyApprovalによって承認されたときに `convergePolicy`メソッドを呼び出す
-			})
-			it('Change the current Policy by passing a Policy.', async () => {
-				// ポリシーを渡すことにより、現在のポリシーを変更します。
-			})
-			it('Killing another Policy when changing Policy.', async () => {
-				// ポリシーを変更するときに別のポリシーを強制終了する
-			})
-			it('Should fail when a call from other than Policy.', async () => {
-				// ポリシー以外からの呼び出し時に失敗する必要があります
-			})
-		})
+		// Describe('PolicyFactory; convergePolicy', () => {
+		// 	it('Calling `convergePolicy` method when approved by Policy.policyApproval.', async () => {
+		// 		// Policy.policyApprovalによって承認されたときに `convergePolicy`メソッドを呼び出す
+		// 	})
+		// 	it('Change the current Policy by passing a Policy.', async () => {
+		// 		// ポリシーを渡すことにより、現在のポリシーを変更します。
+		// 	})
+		// 	it('Killing another Policy when changing Policy.', async () => {
+		// 		// ポリシーを変更するときに別のポリシーを強制終了する
+		// 	})
+		// 	it('Should fail when a call from other than Policy.', async () => {
+		// 		// ポリシー以外からの呼び出し時に失敗する必要があります
+		// 	})
+		// })
 	}
 )
