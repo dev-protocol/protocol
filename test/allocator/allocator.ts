2230a72023f8901b8ae6ba80a8934415ee732206
aggre <hiroyuki.aggre@gmail.com>
2020-01-18 11:27:44 +0900

    withdraw's test cases move to Withdraw Contract's test

--- test/allocator/allocator.ts
+++ test/allocator/allocator.ts
@@ -401,42 +401,6 @@ contract('Allocator', ([deployer]) => {
 		})
 	})
 
-	describe('Allocator; withdraw', () => {
-		describe('Allocator; Withdraw is mint', () => {
-			it('Withdraw mints an ERC20 token specified in the State Contract')
-		})
-
-		describe('Allocator; Withdrawable amount', () => {
-			it(
-				'The withdrawable amount each holder is the number multiplied the balance of the price per Property Contract and the Property Contract of the sender'
-			)
-
-			it(
-				'The withdrawal amount is always the full amount of the withdrawable amount'
-			)
-
-			it('When the withdrawable amount is 0, the withdrawal amount is 0')
-		})
-
-		describe('Allocator; Alice has sent 800 out of 1000 tokens to Bob. Bob has increased from 200 tokens to 1000 tokens. Price is 100', () => {
-			describe('Allocator; Before increment', () => {
-				it(`Alice's withdrawable amount is ${1000 * 100}`)
-
-				it(`Bob's withdrawable amount is ${200 * 100}`)
-			})
-
-			describe('Allocator; After increment; New price is 120', () => {
-				it(`Alice's withdrawable amount is ${1000 * 100 + 200 * 120}`)
-
-				it(`Bob's withdrawable amount is ${200 * 100 + 1000 * 120}`)
-			})
-
-			it(
-				'Should fail to call `beforeBalanceChange` when sent from other than Property Contract address'
-			)
-		})
-	})
-
 	describe('Allocator; kill', () => {
 		it('Destruct this contract')
 
--- test/withdraw/withdraw.ts
+++ test/withdraw/withdraw.ts
@@ -1,6 +1,38 @@
 contract('WithdrawTest', () => {
 	describe('Withdraw; withdraw', () => {
-		// TODO
+		describe('Withdraw; Withdraw is mint', () => {
+			it('Withdraw mints an ERC20 token specified in the State Contract')
+		})
+
+		describe('Withdraw; Withdrawable amount', () => {
+			it(
+				'The withdrawable amount each holder is the number multiplied the balance of the price per Property Contract and the Property Contract of the sender'
+			)
+
+			it(
+				'The withdrawal amount is always the full amount of the withdrawable amount'
+			)
+
+			it('When the withdrawable amount is 0, the withdrawal amount is 0')
+		})
+
+		describe('Withdraw; Alice has sent 800 out of 1000 tokens to Bob. Bob has increased from 200 tokens to 1000 tokens. Price is 100', () => {
+			describe('Withdraw; Before increment', () => {
+				it(`Alice's withdrawable amount is ${1000 * 100}`)
+
+				it(`Bob's withdrawable amount is ${200 * 100}`)
+			})
+
+			describe('Withdraw; After increment; New price is 120', () => {
+				it(`Alice's withdrawable amount is ${1000 * 100 + 200 * 120}`)
+
+				it(`Bob's withdrawable amount is ${200 * 100 + 1000 * 120}`)
+			})
+
+			it(
+				'Should fail to call `beforeBalanceChange` when sent from other than Property Contract address'
+			)
+		})
 	})
 	describe('Withdraw; beforeBalanceChange', () => {
 		// TODO
