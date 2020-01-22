c980f5409633be8e03f87c2e318f40f6816127c1
Akira-Taniguchi <einstein19821108>
2020-01-22 22:12:17 +0900

    add constract test

--- test/market/market.ts
+++ test/market/market.ts
@@ -1,225 +1,285 @@
-contract('MarketTest', ([deployer]) => {
-	const marketContract = artifacts.require('Market')
-	const marketFactoryContract = artifacts.require('MarketFactory')
-	const marketGroupContract = artifacts.require('MarketGroup')
-	const marketTest1Contract = artifacts.require('MarketTest1')
-	const DevContract = artifacts.require('Dev')
-	const addressConfigContract = artifacts.require('AddressConfig')
-	const policyContract = artifacts.require('PolicyTest1')
-	const policyFactoryContract = artifacts.require('PolicyFactory')
-	const voteTimesContract = artifacts.require('VoteTimes')
-	const voteTimesStorageContract = artifacts.require('VoteTimesStorage')
-	const propertyFactoryContract = artifacts.require('PropertyFactory')
-	const propertyGroupContract = artifacts.require('PropertyGroup')
-	const lockupContract = artifacts.require('Lockup')
-	const lockupStorageContract = artifacts.require('LockupStorage')
-	const allocatorContract = artifacts.require('Allocator')
-	const policyGroupContract = artifacts.require('PolicyGroup')
-	const policySetContract = artifacts.require('PolicySet')
-	const decimalsLibrary = artifacts.require('Decimals')
-	describe('Market; schema', () => {
-		it('Get Schema of mapped Behavior Contract')
-	})
+import {DevProtocolInstance, UserInstance} from '../test-lib/instance'
+import {validateAddressErrorMessage} from '../test-lib/utils'
 
-	describe('Market; authenticate', () => {
-		it('Proxy to mapped Behavior Contract')
-
-		it(
-			'Should fail to run when sent from other than the owner of Property Contract'
-		)
-
-		it(
-			'Should fail to the transaction if the second argument as ID and a Metrics Contract exists with the same ID.'
-		)
-	})
-
-	describe('Market; authenticatedCallback', () => {
-		it('Create a new Metrics Contract')
-
-		it(
-			'Market Contract address and Property Contract address are mapped to the created Metrics Contract'
-		)
-
-		it(
-			'Should fail to create a new Metrics Contract when sent from non-Behavior Contract'
-		)
-	})
-
-	describe('Market; calculate', () => {
-		it('Proxy to mapped Behavior Contract')
-	})
-
-	// eslint-disable-next-line no-warning-comments
-	// TODO vote interface wa changed, I will create this test case later
-	describe('Market; vote', () => {
-		let marketFactory: any
-		let marketGroup: any
-		let dev: any
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		let market: any
-		let addressConfig: any
-		let policy: any
-		let policyFactory: any
-		let voteTimes: any
-		let voteTimesStorage: any
-		let propertyFactory: any
-		let propertyGroup: any
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		let propertyAddress: any
-		let behavior: any
-		let allocator: any
-		let lockup: any
-		let policyGroup: any
-		let policySet: any
-		before(async () => {
-			addressConfig = await addressConfigContract.new({from: deployer})
-			dev = await DevContract.new(addressConfig.address, {from: deployer})
-			await addressConfig.setToken(dev.address, {from: deployer})
-			marketFactory = await marketFactoryContract.new(addressConfig.address, {
-				from: deployer
+contract(
+	'MarketTest',
+	([deployer, marketFactory, behavuor, user, metrics, allocator]) => {
+		const marketContract = artifacts.require('Market')
+		describe('Market; constructor', () => {
+			const dev = new DevProtocolInstance(deployer)
+			const userInstance = new UserInstance(dev, user)
+			beforeEach(async () => {
+				await dev.generateAddressConfig()
 			})
-			marketGroup = await marketGroupContract.new(addressConfig.address, {
-				from: deployer
+			it('market factory以外からは作成できない', async () => {
+				await dev.addressConfig.setMarketFactory(marketFactory)
+				const result = await marketContract
+					.new(dev.addressConfig.address, behavuor, {from: deployer})
+					.catch((err: Error) => err)
+				validateAddressErrorMessage(result as Error)
 			})
-			await marketGroup.createStorage()
-			const decimals = await decimalsLibrary.new({from: deployer})
-			await allocatorContract.link('Decimals', decimals.address)
-			allocator = await allocatorContract.new(addressConfig.address, {
-				from: deployer
+			it('各プロパティがセットされている', async () => {
+				await Promise.all([
+					dev.generatePolicyFactory(),
+					dev.generatePolicyGroup(),
+					dev.generatePolicySet()
+				])
+				await dev.addressConfig.setMarketFactory(marketFactory)
+				const iPolicyInstance = await userInstance.getPolicy('PolicyTest1')
+				await dev.policyFactory.create(iPolicyInstance.address)
+				const market = await marketContract.new(
+					dev.addressConfig.address,
+					behavuor,
+					{from: marketFactory}
+				)
+				expect(await market.behavior()).to.be.equal(behavuor)
+				expect(await market.enabled()).to.be.equal(false)
 			})
-			await addressConfig.setAllocator(allocator.address, {
-				from: deployer
+		})
+		describe('Market; schema', () => {
+			const dev = new DevProtocolInstance(deployer)
+			const userInstance = new UserInstance(dev, user)
+			it('Get Schema of mapped Behavior Contract', async () => {
+				await dev.generateAddressConfig()
+				await Promise.all([
+					dev.generatePolicyFactory(),
+					dev.generatePolicyGroup(),
+					dev.generatePolicySet()
+				])
+				await dev.addressConfig.setMarketFactory(marketFactory)
+				const iPolicyInstance = await userInstance.getPolicy('PolicyTest1')
+				await dev.policyFactory.create(iPolicyInstance.address)
+				const behavuor = await userInstance.getMarket('MarketTest1')
+				const market = await marketContract.new(
+					dev.addressConfig.address,
+					behavuor.address,
+					{from: marketFactory}
+				)
+				expect(await market.schema()).to.be.equal('[]')
 			})
-			await lockupContract.link('Decimals', decimals.address)
-			lockup = await lockupContract.new(addressConfig.address, {from: deployer})
-			await addressConfig.setLockup(lockup.address, {
-				from: deployer
+		})
+		describe('Market; calculate', () => {
+			const dev = new DevProtocolInstance(deployer)
+			const userInstance = new UserInstance(dev, user)
+			it('Proxy to mapped Behavior Contract', async () => {
+				await dev.generateAddressConfig()
+				await Promise.all([
+					dev.generatePolicyFactory(),
+					dev.generatePolicyGroup(),
+					dev.generatePolicySet()
+				])
+				await dev.addressConfig.setMarketFactory(marketFactory)
+				const iPolicyInstance = await userInstance.getPolicy('PolicyTest1')
+				await dev.policyFactory.create(iPolicyInstance.address)
+				const behavuor = await userInstance.getMarket('MarketTest3')
+				const market = await marketContract.new(
+					dev.addressConfig.address,
+					behavuor.address,
+					{from: marketFactory}
+				)
+				await dev.addressConfig.setAllocator(allocator)
+				const result = await market.calculate(metrics, 0, 100, {
+					from: allocator
+				})
+				console.log(result.receipt.rawLogs)
 			})
-			const lockupStorage = await lockupStorageContract.new(
-				addressConfig.address,
-				{from: deployer}
+		})
+		describe('Market; authenticate', () => {
+			it('Proxy to mapped Behavior Contract')
+
+			it(
+				'Should fail to run when sent from other than the owner of Property Contract'
 			)
-			await lockupStorage.createStorage()
-			await addressConfig.setLockupStorage(lockupStorage.address, {
-				from: deployer
-			})
-			voteTimes = await voteTimesContract.new(addressConfig.address, {
-				from: deployer
-			})
-			voteTimesStorage = await voteTimesStorageContract.new(
-				addressConfig.address,
-				{
-					from: deployer
-				}
+
+			it(
+				'Should fail to the transaction if the second argument as ID and a Metrics Contract exists with the same ID.'
 			)
-			await voteTimesStorage.createStorage()
-			await addressConfig.setVoteTimesStorage(voteTimesStorage.address, {
-				from: deployer
-			})
-			await addressConfig.setMarketFactory(marketFactory.address, {
-				from: deployer
-			})
-			await addressConfig.setMarketGroup(marketGroup.address, {from: deployer})
-			await addressConfig.setVoteTimes(voteTimes.address, {
-				from: deployer
-			})
-			propertyFactory = await propertyFactoryContract.new(
-				addressConfig.address,
-				{from: deployer}
+		})
+
+		describe('Market; authenticatedCallback', () => {
+			it('Create a new Metrics Contract')
+
+			it(
+				'Market Contract address and Property Contract address are mapped to the created Metrics Contract'
 			)
-			await addressConfig.setPropertyFactory(propertyFactory.address, {
-				from: deployer
-			})
-			propertyGroup = await propertyGroupContract.new(addressConfig.address, {
-				from: deployer
-			})
-			await propertyGroup.createStorage()
-			await addressConfig.setPropertyGroup(propertyGroup.address, {
-				from: deployer
-			})
-			await policyContract.link('Decimals', decimals.address)
-			policy = await policyContract.new({from: deployer})
-			policyGroup = await policyGroupContract.new(addressConfig.address, {
-				from: deployer
-			})
-			policyGroup.createStorage()
-			await addressConfig.setPolicyGroup(policyGroup.address, {
-				from: deployer
-			})
-			policySet = await policySetContract.new(addressConfig.address, {
-				from: deployer
-			})
-			policySet.createStorage()
-			await addressConfig.setPolicySet(policySet.address, {
-				from: deployer
-			})
-			policyFactory = await policyFactoryContract.new(addressConfig.address, {
-				from: deployer
-			})
-			await addressConfig.setPolicyFactory(policyFactory.address, {
-				from: deployer
-			})
-			await policyFactory.create(policy.address)
-			behavior = await marketTest1Contract.new(addressConfig.address, {
-				from: deployer
-			})
-			let result = await marketFactory.create(behavior.address)
-			const marketAddress = await result.logs.filter(
-				(e: {event: string}) => e.event === 'Create'
-			)[0].args._market
-			// eslint-disable-next-line @typescript-eslint/await-thenable
-			market = await marketContract.at(marketAddress)
-			result = await propertyFactory.create('sample', 'SAMPLE', deployer, {
-				from: deployer
-			})
-			propertyAddress = await result.logs.filter(
-				(e: {event: string}) => e.event === 'Create'
-			)[0].args._property
+
+			it(
+				'Should fail to create a new Metrics Contract when sent from non-Behavior Contract'
+			)
 		})
 
-		it(
-			'Total value of votes for and against, votes are the number of sent DEVs'
-		)
-		it(
-			'Creating a market contract from other than a factory results in an error'
-		)
+		// eslint-disable-next-line no-warning-comments
+		// TODO vote interface wa changed, I will create this test case later
+		describe('Market; vote', () => {
+			// Let marketFactory: any
+			// let marketGroup: any
+			// let dev: any
+			// // eslint-disable-next-line @typescript-eslint/no-unused-vars
+			// let market: any
+			// let addressConfig: any
+			// let policy: any
+			// let policyFactory: any
+			// let voteTimes: any
+			// let voteTimesStorage: any
+			// let propertyFactory: any
+			// let propertyGroup: any
+			// // eslint-disable-next-line @typescript-eslint/no-unused-vars
+			// let propertyAddress: any
+			// let behavior: any
+			// let allocator: any
+			// let lockup: any
+			// let policyGroup: any
+			// let policySet: any
+			before(async () => {
+				// AddressConfig = await addressConfigContract.new({from: deployer})
+				// dev = await DevContract.new(addressConfig.address, {from: deployer})
+				// await addressConfig.setToken(dev.address, {from: deployer})
+				// marketFactory = await marketFactoryContract.new(addressConfig.address, {
+				// 	from: deployer
+				// })
+				// marketGroup = await marketGroupContract.new(addressConfig.address, {
+				// 	from: deployer
+				// })
+				// await marketGroup.createStorage()
+				// const decimals = await decimalsLibrary.new({from: deployer})
+				// await allocatorContract.link('Decimals', decimals.address)
+				// allocator = await allocatorContract.new(addressConfig.address, {
+				// 	from: deployer
+				// })
+				// await addressConfig.setAllocator(allocator.address, {
+				// 	from: deployer
+				// })
+				// await lockupContract.link('Decimals', decimals.address)
+				// lockup = await lockupContract.new(addressConfig.address, {from: deployer})
+				// await addressConfig.setLockup(lockup.address, {
+				// 	from: deployer
+				// })
+				// const lockupStorage = await lockupStorageContract.new(
+				// 	addressConfig.address,
+				// 	{from: deployer}
+				// )
+				// await lockupStorage.createStorage()
+				// await addressConfig.setLockupStorage(lockupStorage.address, {
+				// 	from: deployer
+				// })
+				// voteTimes = await voteTimesContract.new(addressConfig.address, {
+				// 	from: deployer
+				// })
+				// voteTimesStorage = await voteTimesStorageContract.new(
+				// 	addressConfig.address,
+				// 	{
+				// 		from: deployer
+				// 	}
+				// )
+				// await voteTimesStorage.createStorage()
+				// await addressConfig.setVoteTimesStorage(voteTimesStorage.address, {
+				// 	from: deployer
+				// })
+				// await addressConfig.setMarketFactory(marketFactory.address, {
+				// 	from: deployer
+				// })
+				// await addressConfig.setMarketGroup(marketGroup.address, {from: deployer})
+				// await addressConfig.setVoteTimes(voteTimes.address, {
+				// 	from: deployer
+				// })
+				// propertyFactory = await propertyFactoryContract.new(
+				// 	addressConfig.address,
+				// 	{from: deployer}
+				// )
+				// await addressConfig.setPropertyFactory(propertyFactory.address, {
+				// 	from: deployer
+				// })
+				// propertyGroup = await propertyGroupContract.new(addressConfig.address, {
+				// 	from: deployer
+				// })
+				// await propertyGroup.createStorage()
+				// await addressConfig.setPropertyGroup(propertyGroup.address, {
+				// 	from: deployer
+				// })
+				// await policyContract.link('Decimals', decimals.address)
+				// policy = await policyContract.new({from: deployer})
+				// policyGroup = await policyGroupContract.new(addressConfig.address, {
+				// 	from: deployer
+				// })
+				// policyGroup.createStorage()
+				// await addressConfig.setPolicyGroup(policyGroup.address, {
+				// 	from: deployer
+				// })
+				// policySet = await policySetContract.new(addressConfig.address, {
+				// 	from: deployer
+				// })
+				// policySet.createStorage()
+				// await addressConfig.setPolicySet(policySet.address, {
+				// 	from: deployer
+				// })
+				// policyFactory = await policyFactoryContract.new(addressConfig.address, {
+				// 	from: deployer
+				// })
+				// await addressConfig.setPolicyFactory(policyFactory.address, {
+				// 	from: deployer
+				// })
+				// await policyFactory.create(policy.address)
+				// behavior = await marketTest1Contract.new(addressConfig.address, {
+				// 	from: deployer
+				// })
+				// let result = await marketFactory.create(behavior.address)
+				// const marketAddress = await result.logs.filter(
+				// 	(e: {event: string}) => e.event === 'Create'
+				// )[0].args._market
+				// // eslint-disable-next-line @typescript-eslint/await-thenable
+				// market = await marketContract.at(marketAddress)
+				// result = await propertyFactory.create('sample', 'SAMPLE', deployer, {
+				// 	from: deployer
+				// })
+				// propertyAddress = await result.logs.filter(
+				// 	(e: {event: string}) => e.event === 'Create'
+				// )[0].args._property
+			})
 
-		it('When total votes for more than 10% of the total supply of DEV are obtained, this Market Contract is enabled', async () => {
-			// eslint-disable-next-line no-warning-comments
-			// TODO PolicyTest1, VoteCounter, VoteCounterStorage
-			// await dummyDEV.approve(lockup.address, 10000, {from: deployer})
-			// await lockup.lockup(propertyAddress, 10000, {from: deployer})
-			// await market.vote(propertyAddress, true, {from: deployer})
-			// const isEnable = await market.enabled({from: deployer})
-			// expect(isEnable).to.be.equal(true)
-		})
+			it(
+				'Total value of votes for and against, votes are the number of sent DEVs'
+			)
+			it(
+				'Creating a market contract from other than a factory results in an error'
+			)
 
-		it('Should fail to vote when already determined enabled', async () => {
-			// Await dummyDEV.approve(market.address, 100000, {from: deployer})
-			// await market.vote(10000, {from: deployer})
-			// const isEnable = await market.enabled({from: deployer})
-			// expect(isEnable).to.be.equal(true)
-			// const result = await market
-			// 	.vote(100, {from: deployer})
-			// 	.catch((err: Error) => err)
-			// expect((result as Error).message).to.be.equal(
-			// 	'Returned error: VM Exception while processing transaction: revert market is already enabled -- Reason given: market is already enabled.'
-			// )
-		})
+			it('When total votes for more than 10% of the total supply of DEV are obtained, this Market Contract is enabled', async () => {
+				// eslint-disable-next-line no-warning-comments
+				// TODO PolicyTest1, VoteCounter, VoteCounterStorage
+				// await dummyDEV.approve(lockup.address, 10000, {from: deployer})
+				// await lockup.lockup(propertyAddress, 10000, {from: deployer})
+				// await market.vote(propertyAddress, true, {from: deployer})
+				// const isEnable = await market.enabled({from: deployer})
+				// expect(isEnable).to.be.equal(true)
+			})
 
-		it('Vote decrease the number of sent DEVs from voter owned DEVs', async () => {
-			// Await dummyDEV.approve(market.address, 100, {from: deployer})
-			// await market.vote(100, {from: deployer})
-			// const ownedDEVs = await dummyDEV.balanceOf(deployer, {from: deployer})
-			// expect(ownedDEVs.toNumber()).to.be.equal(9900)
-		})
+			it('Should fail to vote when already determined enabled', async () => {
+				// Await dummyDEV.approve(market.address, 100000, {from: deployer})
+				// await market.vote(10000, {from: deployer})
+				// const isEnable = await market.enabled({from: deployer})
+				// expect(isEnable).to.be.equal(true)
+				// const result = await market
+				// 	.vote(100, {from: deployer})
+				// 	.catch((err: Error) => err)
+				// expect((result as Error).message).to.be.equal(
+				// 	'Returned error: VM Exception while processing transaction: revert market is already enabled -- Reason given: market is already enabled.'
+				// )
+			})
 
-		it('Vote decrease the number of sent DEVs from DEVtoken totalSupply', async () => {
-			// Await dummyDEV.approve(market.address, 100, {from: deployer})
-			// await market.vote(100, {from: deployer})
-			// const DEVsTotalSupply = await dummyDEV.totalSupply({from: deployer})
-			// expect(DEVsTotalSupply.toNumber()).to.be.equal(9900)
+			it('Vote decrease the number of sent DEVs from voter owned DEVs', async () => {
+				// Await dummyDEV.approve(market.address, 100, {from: deployer})
+				// await market.vote(100, {from: deployer})
+				// const ownedDEVs = await dummyDEV.balanceOf(deployer, {from: deployer})
+				// expect(ownedDEVs.toNumber()).to.be.equal(9900)
+			})
+
+			it('Vote decrease the number of sent DEVs from DEVtoken totalSupply', async () => {
+				// Await dummyDEV.approve(market.address, 100, {from: deployer})
+				// await market.vote(100, {from: deployer})
+				// const DEVsTotalSupply = await dummyDEV.totalSupply({from: deployer})
+				// expect(DEVsTotalSupply.toNumber()).to.be.equal(9900)
+			})
+			it('voting deadline is over')
 		})
-		it('voting deadline is over')
-	})
-})
+	}
+)
