937aca36acb52bff40a8116250bdd2908a444e80
aggre <hiroyuki.aggre@gmail.com>
2020-02-06 15:26:36 +0900

    change to view

--- contracts/src/lockup/Lockup.sol
+++ contracts/src/lockup/Lockup.sol
@@ -138,7 +138,7 @@ contract Lockup is Pausable, UsingConfig, UsingValidator, Killable {
 		require(erc20.mint(msg.sender, value), "dev mint failed");
 	}
 
-	function getAllValue() external returns (uint256) {
+	function getAllValue() external view returns (uint256) {
 		return getStorage().getAllValue();
 	}
 
