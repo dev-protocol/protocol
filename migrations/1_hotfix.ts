9cdaf2de15626b7314888150f6ad91cfbc9cf474
aggre <hiroyuki.aggre@gmail.com>
2020-02-07 13:06:57 +0900

    link to Decimals lib

--- migrations/1_hotfix.ts
+++ migrations/1_hotfix.ts
@@ -5,6 +5,12 @@ const handler = function(deployer, network) {
 		return
 	}
 
+	const decimals = artifacts.require('Decimals')
+
+	deployer.deploy(decimals)
+	deployer.link(decimals, artifacts.require('Allocator'))
+	deployer.link(decimals, artifacts.require('Lockup'))
+
 	deployer.deploy(artifacts.require('Allocator'), ADDRESS_CONFIG)
 	deployer.deploy(artifacts.require('Lockup'), ADDRESS_CONFIG)
 	deployer.deploy(artifacts.require('LockupStorage'), ADDRESS_CONFIG)
