067c002774618589e2c65a4369339c7091ccfa08
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-05-26 10:56:31 +0900

    add transferFrom function

--- contracts/src/property/Property.sol
+++ contracts/src/property/Property.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.5.0;
 
+import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // prettier-ignore
 import {ERC20Detailed} from "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
@@ -10,6 +11,7 @@ import {Lockup} from "contracts/src/lockup/Lockup.sol";
 
 
 contract Property is ERC20, ERC20Detailed, UsingConfig, UsingValidator {
+	using SafeMath for uint256;
 	uint8 private constant _property_decimals = 18;
 	uint256 private constant _supply = 10000000000000000000000000;
 	address public author;
@@ -43,6 +45,22 @@ contract Property is ERC20, ERC20Detailed, UsingConfig, UsingValidator {
 			_to
 		);
 		_transfer(msg.sender, _to, _value);
+		return true;
+	}
+
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+		addressValidator().validateIllegalAddress(_from);
+		addressValidator().validateIllegalAddress(_to);
+		require(_value != 0, "illegal transfer value");
+
+		Allocator(config().allocator()).beforeBalanceChange(
+			address(this),
+			_from,
+			_to
+		);
+		_transfer(_from, _to, _value);
+		uint256 allowanceAmount = allowance(_from, msg.sender);
+		_approve(_from, msg.sender, allowanceAmount.sub(_value, "ERC20: transfer amount exceeds allowance"));
 		return true;
 	}
 
