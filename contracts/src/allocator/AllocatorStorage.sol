d2e2e5d713a2f71ecf5a1e9ae9a50a772a309f5a
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-06-19 12:31:02 +0900

    update by lint

--- contracts/src/allocator/AllocatorStorage.sol
+++ contracts/src/allocator/AllocatorStorage.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.5.0;
 import {UsingStorage} from "contracts/src/common/storage/UsingStorage.sol";
 
 // solium-disable-next-line no-empty-blocks
-contract AllocatorStorage is UsingStorage {}
+contract AllocatorStorage is UsingStorage {
+
+}
