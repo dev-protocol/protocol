835240220dc06f4dc404a610be14b7819dff2098
aggre <hiroyuki.aggre@gmail.com>
2020-07-06 15:52:52 +0900

    add step that update DIP4GenesisBlock

--- scripts/deploy-reduce-gas-lockup.ts
+++ scripts/deploy-reduce-gas-lockup.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 const CONFIG = '0x1D415aa39D647834786EB9B5a333A50e9935b796'
+const DEPLOYED_BLOCK = '10358615'
 
 const handler = async (
 	callback: (err: Error | null) => void
@@ -17,6 +18,9 @@ const handler = async (
 	// Deploy new Lockup and new Withdraw
 	const nextLockup = await artifacts.require('Lockup').new(config.address)
 	const nextWithdraw = await artifacts.require('Withdraw').new(config.address)
+
+	// Update DIP4GenesisBlock
+	await nextLockup.setDIP4GenesisBlock(DEPLOYED_BLOCK)
 
 	// Pause current Lockup contract and Withdraw contract
 	await oldLockup.pause()
