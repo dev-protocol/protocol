1dd86f408f190951336213b97a718847bf8e9389
aggre <hiroyuki.aggre@gmail.com>
2020-07-06 15:48:16 +0900

    add deploy script

---
+++ scripts/deploy-reduce-gas-lockup.ts
@@ -0,0 +1,32 @@
+/* eslint-disable no-undef */
+const CONFIG = '0x1D415aa39D647834786EB9B5a333A50e9935b796'
+
+const handler = async (
+	callback: (err: Error | null) => void
+): Promise<void> => {
+	// Generate AddressConfig contract
+	const [config] = await Promise.all([
+		artifacts.require('AddressConfig').at(CONFIG),
+	])
+	// Generate current Lockup contract and Withdraw contract
+	const [oldLockup, oldWithdraw] = await Promise.all([
+		artifacts.require('Lockup').at(await config.lockup()),
+		artifacts.require('Withdraw').at(await config.withdraw()),
+	])
+
+	// Deploy new Lockup and new Withdraw
+	const nextLockup = await artifacts.require('Lockup').new(config.address)
+	const nextWithdraw = await artifacts.require('Withdraw').new(config.address)
+
+	// Pause current Lockup contract and Withdraw contract
+	await oldLockup.pause()
+	await oldWithdraw.pause()
+
+	// Enable new Lockup and new Withdraw
+	await config.setLockup(nextLockup.address)
+	await config.setWithdraw(nextWithdraw.address)
+
+	callback(null)
+}
+
+export = handler
