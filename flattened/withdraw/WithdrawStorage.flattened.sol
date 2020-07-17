e67c48034d3f317298dad4dfc3fba4df9b165a03
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-07-17 12:25:47 +0900

    update by lint

--- flattened/withdraw/WithdrawStorage.flattened.sol
+++ flattened/withdraw/WithdrawStorage.flattened.sol
@@ -1,4 +1,3 @@
-
 // File: contracts/src/common/interface/IGroup.sol
 
 pragma solidity ^0.5.0;
@@ -16,7 +15,6 @@ contract IGroup {
 // File: contracts/src/common/validate/AddressValidator.sol
 
 pragma solidity ^0.5.0;
-
 
 contract AddressValidator {
 	string constant errorMessage = "this is illegal address";
@@ -77,7 +75,6 @@ pragma solidity ^0.5.0;
 
 // prettier-ignore
 
-
 contract UsingValidator {
 	AddressValidator private _validator;
 
@@ -105,19 +102,20 @@ pragma solidity ^0.5.0;
  * This contract is only required for intermediate, library-like contracts.
  */
 contract Context {
-    // Empty internal constructor, to prevent people from mistakenly deploying
-    // an instance of this contract, which should be used via inheritance.
-    constructor () internal { }
-    // solhint-disable-previous-line no-empty-blocks
+	// Empty internal constructor, to prevent people from mistakenly deploying
+	// an instance of this contract, which should be used via inheritance.
+	constructor() internal {}
 
-    function _msgSender() internal view returns (address payable) {
-        return msg.sender;
-    }
+	// solhint-disable-previous-line no-empty-blocks
 
-    function _msgData() internal view returns (bytes memory) {
-        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
-        return msg.data;
-    }
+	function _msgSender() internal view returns (address payable) {
+		return msg.sender;
+	}
+
+	function _msgData() internal view returns (bytes memory) {
+		this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+		return msg.data;
+	}
 }
 
 // File: @openzeppelin/contracts/ownership/Ownable.sol
@@ -134,69 +132,75 @@ pragma solidity ^0.5.0;
  * the owner.
  */
 contract Ownable is Context {
-    address private _owner;
+	address private _owner;
 
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+	event OwnershipTransferred(
+		address indexed previousOwner,
+		address indexed newOwner
+	);
 
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    constructor () internal {
-        address msgSender = _msgSender();
-        _owner = msgSender;
-        emit OwnershipTransferred(address(0), msgSender);
-    }
+	/**
+	 * @dev Initializes the contract setting the deployer as the initial owner.
+	 */
+	constructor() internal {
+		address msgSender = _msgSender();
+		_owner = msgSender;
+		emit OwnershipTransferred(address(0), msgSender);
+	}
 
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view returns (address) {
-        return _owner;
-    }
+	/**
+	 * @dev Returns the address of the current owner.
+	 */
+	function owner() public view returns (address) {
+		return _owner;
+	}
 
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(isOwner(), "Ownable: caller is not the owner");
-        _;
-    }
+	/**
+	 * @dev Throws if called by any account other than the owner.
+	 */
+	modifier onlyOwner() {
+		require(isOwner(), "Ownable: caller is not the owner");
+		_;
+	}
 
-    /**
-     * @dev Returns true if the caller is the current owner.
-     */
-    function isOwner() public view returns (bool) {
-        return _msgSender() == _owner;
-    }
+	/**
+	 * @dev Returns true if the caller is the current owner.
+	 */
+	function isOwner() public view returns (bool) {
+		return _msgSender() == _owner;
+	}
 
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    function renounceOwnership() public onlyOwner {
-        emit OwnershipTransferred(_owner, address(0));
-        _owner = address(0);
-    }
+	/**
+	 * @dev Leaves the contract without owner. It will not be possible to call
+	 * `onlyOwner` functions anymore. Can only be called by the current owner.
+	 *
+	 * NOTE: Renouncing ownership will leave the contract without an owner,
+	 * thereby removing any functionality that is only available to the owner.
+	 */
+	function renounceOwnership() public onlyOwner {
+		emit OwnershipTransferred(_owner, address(0));
+		_owner = address(0);
+	}
 
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address newOwner) public onlyOwner {
-        _transferOwnership(newOwner);
-    }
+	/**
+	 * @dev Transfers ownership of the contract to a new account (`newOwner`).
+	 * Can only be called by the current owner.
+	 */
+	function transferOwnership(address newOwner) public onlyOwner {
+		_transferOwnership(newOwner);
+	}
 
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     */
-    function _transferOwnership(address newOwner) internal {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        emit OwnershipTransferred(_owner, newOwner);
-        _owner = newOwner;
-    }
+	/**
+	 * @dev Transfers ownership of the contract to a new account (`newOwner`).
+	 */
+	function _transferOwnership(address newOwner) internal {
+		require(
+			newOwner != address(0),
+			"Ownable: new owner is the zero address"
+		);
+		emit OwnershipTransferred(_owner, newOwner);
+		_owner = newOwner;
+	}
 }
 
 // File: contracts/src/common/storage/EternalStorage.sol
@@ -309,8 +313,6 @@ contract EternalStorage {
 
 pragma solidity ^0.5.0;
 
-
-
 contract UsingStorage is Ownable {
 	address private _storage;
 
@@ -367,9 +369,6 @@ contract Killable {
 // File: contracts/src/common/config/AddressConfig.sol
 
 pragma solidity ^0.5.0;
-
-
-
 
 contract AddressConfig is Ownable, UsingValidator, Killable {
 	address public token = 0x98626E2C9231f03504273d55f397409deFD4a093;
@@ -484,7 +483,6 @@ contract AddressConfig is Ownable, UsingValidator, Killable {
 
 pragma solidity ^0.5.0;
 
-
 contract UsingConfig {
 	AddressConfig private _config;
 
@@ -504,9 +502,6 @@ contract UsingConfig {
 // File: contracts/src/withdraw/WithdrawStorage.sol
 
 pragma solidity ^0.5.0;
-
-
-
 
 contract WithdrawStorage is UsingStorage, UsingConfig, UsingValidator {
 	// solium-disable-next-line no-empty-blocks
