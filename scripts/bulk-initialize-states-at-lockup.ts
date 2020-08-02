66e88f54769836d043f6712802a3e9175c8e59d1
aggre <hiroyuki.aggre@gmail.com>
2020-08-03 01:22:10 +0900

    WIP: prototype

--- scripts/bulk-initialize-states-at-lockup.ts
+++ scripts/bulk-initialize-states-at-lockup.ts
@@ -1,9 +1,41 @@
+/* eslint-disable no-useless-call */
 /* eslint-disable no-undef */
 import bent from 'bent'
+import Queue from 'p-queue'
 import Web3 from 'web3'
-const {CONFIG} = process.env
+import {Contract} from 'web3-eth-contract/types'
+const {CONFIG, EGS_TOKEN} = process.env
 const {log: ____log} = console
+type GraphQLResponse = {
+	data: {
+		account_lockup: Array<{
+			property_address: string
+			account_address: string
+			lockedup: {
+				block_number: number
+			}
+		}>
+	}
+}
+type EGSResponse = {
+	fast: number
+	fastest: number
+	safeLow: number
+	average: number
+	block_time: number
+	blockNum: number
+	speed: number
+	safeLowWait: number
+	avgWait: number
+	fastWait: number
+	fastestWait: number
+}
+type GasPriceFetcher = () => Promise<string>
 
+const graphql = () =>
+	bent('https://api.devprtcl.com/v1/graphql', 'POST', 'json')
+const ethgas = (token: string) =>
+	bent(`https://ethgasstation.info/api/ethgasAPI.json?api-key=${token}`, 'json')
 const prepare = async (configAddress: string) => {
 	const [config] = await Promise.all([
 		artifacts.require('AddressConfig').at(configAddress),
@@ -15,39 +47,162 @@ const prepare = async (configAddress: string) => {
 	return contract
 }
 
-type GraphQLResponse = {
-	data: {
-		property_factory_create: Array<{
-			property: string
-			lockedup: Array<{block_number: number; from_address: string}>
-		}>
-	}
-}
-const fetch = async (): Promise<GraphQLResponse> =>
-	bent('POST', 'json')('https://api.devprtcl.com/v1/graphql', {
+const createGraphQLFetcher = (
+	fetcher: bent.RequestFunction<bent.ValidResponse>
+) => async (offset = 0): Promise<GraphQLResponse> =>
+	fetcher('/', {
 		query: `{
-			property_factory_create(order_by: {allocation_aggregate: {sum: {result: desc_nulls_last}}}) {
-				property
-				lockedup(distinct_on: [from_address], order_by: [{from_address: desc}, {block_number: desc}]) {
+			account_lockup(
+				offset: ${offset},
+				distinct_on: [account_address, property_address],
+				order_by: [
+					{account_address: desc_nulls_last},
+					{property_address: desc_nulls_last}
+					{lockedup: {block_number: desc_nulls_last}}
+			  	]
+			) {
+				property_address
+				account_address
+				lockedup {
 					block_number
-					from_address
 				}
 			}
 		}`,
 	}).then((r) => (r as unknown) as GraphQLResponse)
+const createEGSFetcher = (
+	fetcher: bent.RequestFunction<bent.ValidResponse>
+) => async (): Promise<EGSResponse> =>
+	fetcher('').then((r) => (r as unknown) as EGSResponse)
+const createGetStorageLastCumulativeGlobalReward = (lockup: Contract) => (
+	blockNumber?: number
+) => async (property: string, user: string): Promise<string> =>
+	lockup.methods
+		.getStorageLastCumulativeGlobalReward(property, user)
+		.call(undefined, blockNumber)
+const createGetStorageLastCumulativeLockedUpAndBlock = (lockup: Contract) => (
+	blockNumber?: number
+) => async (
+	property: string,
+	user: string
+): Promise<{_cLocked: string; _block: string}> =>
+	lockup.methods
+		.getStorageLastCumulativeLockedUpAndBlock(property, user)
+		.call(undefined, blockNumber)
+const createDifferenceCaller = (lockup: Contract) => (
+	blockNumber?: number
+) => async (property: string): Promise<{_reward: string}> =>
+	lockup.methods.difference(property).call(undefined, blockNumber)
+const createGetCumulativeLockedUpCaller = (lockup: Contract) => (
+	blockNumber?: number
+) => async (property: string, user: string): Promise<{_value: string}> =>
+	lockup.methods
+		.getCumulativeLockedUp(property, user)
+		.call(undefined, blockNumber)
+const createInitializeStatesAtLockup = (lockup: Contract) => (
+	gasPriceFetcher: GasPriceFetcher
+) => (from: string) => async (
+	property: string,
+	user: string,
+	reward: string,
+	cLocked: string,
+	block: string
+	// eslint-disable-next-line max-params
+) =>
+	lockup.methods
+		.initializeStatesAtLockup(property, user, reward, cLocked, block)
+		.send({gasPrice: await gasPriceFetcher(), from})
+const createQueue = (concurrency: number) => new Queue({concurrency})
 
 const handler = async (
 	callback: (err: Error | null) => void
 ): Promise<void> => {
-	if (!CONFIG) {
+	if (!CONFIG || !EGS_TOKEN) {
 		return
 	}
+
+	const [from] = await (web3 as Web3).eth.getAccounts()
 
 	const lockup = await prepare(CONFIG)
 	____log('Generated Lockup contract', lockup.options)
 
-	const data = await fetch()
-	____log('GraphQL fetched', data)
+	const fetchGraphQL = createGraphQLFetcher(graphql())
+	const all = await (async () =>
+		new Promise<GraphQLResponse['data']['account_lockup']>((resolve) => {
+			const f = async (
+				i = 0,
+				prev: GraphQLResponse['data']['account_lockup'] = []
+			): Promise<void> => {
+				const {data} = await fetchGraphQL(i)
+				const {account_lockup: items} = data
+				const next = [...prev, ...items]
+				if (items.length > 0) {
+					f(i + items.length, next).catch(console.error)
+				} else {
+					resolve(next)
+				}
+			}
+
+			f().catch(console.error)
+		}))()
+	____log('GraphQL fetched', all)
+
+	const fetchGasPrice = createEGSFetcher(ethgas(EGS_TOKEN))
+
+	const fetchFastestGasPrice = async () =>
+		fetchGasPrice().then((res) =>
+			(web3 as Web3).utils.toWei(`${res.fastest / 10}`, 'Gwei')
+		)
+	____log(await fetchFastestGasPrice())
+
+	const lastCumulativeGlobalReward = createGetStorageLastCumulativeGlobalReward(
+		lockup
+	)
+	const lastCumulativeLockedUpAndBlock = createGetStorageLastCumulativeLockedUpAndBlock(
+		lockup
+	)
+	const difference = createDifferenceCaller(lockup)
+	const getCumulativeLockedUp = createGetCumulativeLockedUpCaller(lockup)
+	const initializeStatesAtLockup = createInitializeStatesAtLockup(lockup)(
+		fetchFastestGasPrice
+	)(from)
+
+	const targets = all.filter(({lockedup}) => Boolean(lockedup))
+	const filteringTacks = targets.map(
+		({property_address, account_address, ...x}) => async () => {
+			const [cReward, {_cLocked, _block}] = await Promise.all([
+				lastCumulativeGlobalReward()(property_address, account_address),
+				lastCumulativeLockedUpAndBlock()(property_address, account_address),
+			])
+			const skip = [cReward, _cLocked, _block].every((y) => y !== '0')
+			____log('Skip', property_address, account_address)
+			return {property_address, account_address, skip, ...x}
+		}
+	)
+	const shouldInitilizeItems = await createQueue(10)
+		.addAll(filteringTacks)
+		.then((done) => done.filter((x) => !x.skip))
+
+	const initializeTasks = shouldInitilizeItems.map(
+		({property_address, account_address, lockedup}) => async () => {
+			const {block_number} = lockedup
+			const res = await Promise.all([
+				difference(block_number)(property_address),
+				getCumulativeLockedUp(block_number)(property_address, account_address),
+			])
+			const reward = res[0]._reward
+			const cLocked = res[1]._value
+			console.log(property_address, account_address, reward, cLocked)
+			initializeStatesAtLockup(
+				property_address,
+				account_address,
+				reward,
+				cLocked,
+				block_number.toString()
+			).catch(console.error)
+		}
+	)
+
+	createQueue(2).addAll(initializeTasks).catch(console.error)
 
 	callback(null)
 }
