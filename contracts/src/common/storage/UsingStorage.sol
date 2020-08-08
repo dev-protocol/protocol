309f1a6337d65ac66adb10b10b032eaaabcf822d
aggre <hiroyuki.aggre@gmail.com>
2020-08-09 01:05:15 +0900

    add comments for describing the behavior: UsingStorage contract

--- contracts/src/common/storage/UsingStorage.sol
+++ contracts/src/common/storage/UsingStorage.sol
@@ -3,14 +3,23 @@ pragma solidity ^0.5.0;
 import {Ownable} from "@openzeppelin/contracts/ownership/Ownable.sol";
 import {EternalStorage} from "contracts/src/common/storage/EternalStorage.sol";
 
+/**
+ * Module for contrast handling EternalStorage.
+ */
 contract UsingStorage is Ownable {
 	address private _storage;
 
+	/**
+	 * Modifier to verify that EternalStorage is set.
+	 */
 	modifier hasStorage() {
 		require(_storage != address(0), "storage is not setted");
 		_;
 	}
 
+	/**
+	 * Returns the set EternalStorage instance.
+	 */
 	function eternalStorage()
 		internal
 		view
@@ -20,20 +29,36 @@ contract UsingStorage is Ownable {
 		return EternalStorage(_storage);
 	}
 
+	/**
+	 * Returns the set EternalStorage address.
+	 */
 	function getStorageAddress() external view hasStorage returns (address) {
 		return _storage;
 	}
 
+	/**
+	 * Create a new EternalStorage contract.
+	 * This function call will fail if the EternalStorage contract is already set.
+	 * Also, only the owner can execute it.
+	 */
 	function createStorage() external onlyOwner {
 		require(_storage == address(0), "storage is setted");
 		EternalStorage tmp = new EternalStorage();
 		_storage = address(tmp);
 	}
 
+	/**
+	 * Assigns the EternalStorage contract that has already been created.
+	 * Only the owner can execute this function.
+	 */
 	function setStorage(address _storageAddress) external onlyOwner {
 		_storage = _storageAddress;
 	}
 
+	/**
+	 * Delegates the owner of the current EternalStorage contract.
+	 * Only the owner can execute this function.
+	 */
 	function changeOwner(address newOwner) external onlyOwner {
 		EternalStorage(_storage).changeOwner(newOwner);
 	}
