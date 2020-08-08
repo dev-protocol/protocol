c35faa878eb10e3164b6dde8dd89a57b9fe44408
aggre <hiroyuki.aggre@gmail.com>
2020-08-09 08:52:27 +0900

    add comments for describing the behavior: DevMigration contract

--- contracts/src/dev/DevMigration.sol
+++ contracts/src/dev/DevMigration.sol
@@ -5,15 +5,31 @@ import {ERC20Mintable} from "@openzeppelin/contracts/token/ERC20/ERC20Mintable.s
 // prettier-ignore
 import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 
+/**
+ * A contract to migrate two ERC20 tokens.
+ * Used to migrate the legacy DEV tokens to the latest DEV tokens.
+ */
 contract DevMigration {
 	address public legacy;
 	address public next;
 
+	/**
+	 * Initialize the two passed addresses as the source and destination.
+	 */
 	constructor(address _legacy, address _next) public {
 		legacy = _legacy;
 		next = _next;
 	}
 
+	/**
+	 * Migrates tokens.
+	 * The steps: Transfer a sender's all balances of source tokens to this contract and then burn the same amount. Then mint the destination tokens.
+	 * Notes:
+	 * - Approve is required to transfer the balance of the source token.
+	 * - The source tokens must be burnable.
+	 * - The destination tokens must be mintable.
+	 * - This contract needs permission to mint the destination tokens.
+	 */
 	function migrate() external returns (bool) {
 		ERC20Burnable _legacy = ERC20Burnable(legacy);
 		ERC20Mintable _next = ERC20Mintable(next);
