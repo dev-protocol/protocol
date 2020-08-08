8f565c4a03264386a5a5294eb73bba367a5596be
aggre <hiroyuki.aggre@gmail.com>
2020-08-08 23:52:32 +0900

    add comments for describing the behavior: Killable contract

--- contracts/src/common/lifecycle/Killable.sol
+++ contracts/src/common/lifecycle/Killable.sol
@@ -1,12 +1,22 @@
 pragma solidity ^0.5.0;
 
+/**
+ * A module that allows contracts to self-destruct.
+ */
 contract Killable {
 	address payable public _owner;
 
+	/**
+	 * Initialized with the deployer as the owner.
+	 */
 	constructor() internal {
 		_owner = msg.sender;
 	}
 
+	/**
+	 * Self-destruct the contract.
+	 * This function can only be executed by the owner.
+	 */
 	function kill() public {
 		require(msg.sender == _owner, "only owner method");
 		selfdestruct(_owner);
