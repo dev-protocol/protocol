c541407fffbc83a0895c1c63a33e3135d34f0cc4
aggre <aggre@devprtcl.com>
2020-08-18 16:28:47 +0000

    calculate the fastest gas price and use it for transactions

--- scripts/528-change-authenticate-interface.ts
+++ scripts/528-change-authenticate-interface.ts
@@ -1,26 +1,41 @@
 /* eslint-disable no-undef */
+
+import {createFastestGasPriceFetcher} from './lib/ethgas'
+import {ethgas} from './lib/api'
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config()
 const {log: ____log} = console
+const {CONFIG, EGS_TOKEN} = process.env
+const gas = 6721975
 
 const handler = async (
 	callback: (err: Error | null) => void
 ): Promise<void> => {
+	if (!CONFIG || !EGS_TOKEN) {
+		return
+	}
+
+	const fastest = createFastestGasPriceFetcher(ethgas(EGS_TOKEN), web3)
+
 	// Generate current contract
 	const [config] = await Promise.all([
-		artifacts.require('AddressConfig').at(process.env.CONFIG!),
+		artifacts.require('AddressConfig').at(CONFIG),
 	])
 	____log('Generated AddressConfig contract', config.address)
 
 	// Deploy
 	const nextMarketFactory = await artifacts
 		.require('MarketFactory')
-		.new(config.address)
+		.new(config.address, {gasPrice: await fastest(), gas})
 	____log('Deployed the new MarketFactory', nextMarketFactory.address)
 
 	// Enable new Contract
-	await config.setMarketFactory(nextMarketFactory.address)
-	____log('Update MarketFactory address')
+	await config.setMarketFactory(nextMarketFactory.address, {
+		gasPrice: await fastest(),
+		gas,
+	})
+	____log('Updated MarketFactory address')
 	callback(null)
 }
 
