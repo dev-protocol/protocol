7c8a5f216472544b3ec5344658c37735dd9f565a
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-09-04 17:28:08 +0900

    update by lint

--- contracts/src/policy/IPolicyGroup.sol
+++ contracts/src/policy/IPolicyGroup.sol
@@ -4,5 +4,6 @@ import {IGroup} from "contracts/src/common/interface/IGroup.sol";
 
 contract IPolicyGroup is IGroup {
 	function getVotingGroupIndex() external view returns (uint256);
+
 	function incrementVotingGroupIndex() external;
 }
