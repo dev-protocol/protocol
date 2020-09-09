5263756ce192ab3e70b861967b743703353269ea
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-09-09 10:39:26 +0900

    add policy group test

--- test/policy/policy-group.ts
+++ test/policy/policy-group.ts
@@ -8,7 +8,6 @@ import {mine} from '../test-lib/utils/common'
 contract(
 	'PolicyGroupTest',
 	([deployer, policyFactory, dummyPolicyFactory, policy, dummyPolicy]) => {
-		// Const dev = new DevProtocolInstance(deployer)
 		const init1 = async (): Promise<DevProtocolInstance> => {
 			const dev = new DevProtocolInstance(deployer)
 			await dev.generateAddressConfig()
@@ -29,6 +28,14 @@ contract(
 			await dev.generatePolicyFactory()
 			const policy = await dev.getPolicy('PolicyTestForPolicyFactory', deployer)
 			await dev.policyFactory.create(policy.address, {
+				from: deployer,
+			})
+			return dev
+		}
+
+		const init3 = async (): Promise<DevProtocolInstance> => {
+			const dev = await init2()
+			await dev.addressConfig.setPolicyFactory(policyFactory, {
 				from: deployer,
 			})
 			return dev
@@ -64,7 +71,53 @@ contract(
 				validateAddressErrorMessage(result)
 			})
 		})
-		describe('PolicyGroup; setVotingEndBlockNumber,voting', () => {
+		describe('PolicyGroup; addGroup, isGroup', () => {
+			it('When a policy address is specified', async () => {
+				const dev = await init3()
+				const currentPolicy = await dev.addressConfig.policy()
+
+				const result = await dev.policyGroup.isGroup(currentPolicy)
+				expect(result).to.be.equal(true)
+			})
+			it('When the policy address is not specified', async () => {
+				const dev = await init3()
+				const result = await dev.policyGroup.isGroup(dummyPolicy)
+				expect(result).to.be.equal(false)
+			})
+			it('can add new policy', async () => {
+				const dev = await init3()
+				const policy = await dev.getPolicy(
+					'PolicyTestForPolicyFactory',
+					deployer
+				)
+				await dev.policyGroup.addGroup(policy.address, {
+					from: policyFactory,
+				})
+
+				const result = await dev.policyGroup.isGroup(policy.address)
+				expect(result).to.be.equal(true)
+			})
+			it('Existing policy cannot be added', async () => {
+				const dev = await init3()
+				const currentPolicy = await dev.addressConfig.policy()
+				const result = await dev.policyGroup
+					.addGroup(currentPolicy, {
+						from: policyFactory,
+					})
+					.catch((err: Error) => err)
+				validateErrorMessage(result, 'already group')
+			})
+			it('Can not execute addGroup without policyFactory address', async () => {
+				const dev = await init3()
+				const result = await dev.policyGroup
+					.addGroup(dummyPolicy, {
+						from: dummyPolicyFactory,
+					})
+					.catch((err: Error) => err)
+				validateAddressErrorMessage(result)
+			})
+		})
+		describe('PolicyGroup; voting', () => {
 			it('can get to see if you can vote or not.', async () => {
 				const dev = await init2()
 				const policy = await dev.getPolicy(
@@ -85,7 +138,20 @@ contract(
 				expect(voting).to.be.equal(false)
 			})
 		})
+		describe('PolicyGroup; getVotingGroupIndex, incrementVotingGroupIndex', () => {
+			it('The index can be obtained and incremented.', async () => {
+				const dev = new DevProtocolInstance(deployer)
+				await dev.generateAddressConfig()
+				await dev.generatePolicyGroup()
+				let index = await dev.policyGroup.getVotingGroupIndex()
+				expect(index.toNumber()).to.be.equal(0)
+				await dev.policyGroup.incrementVotingGroupIndex()
+				index = await dev.policyGroup.getVotingGroupIndex()
+				expect(index.toNumber()).to.be.equal(1)
+				await dev.policyGroup.incrementVotingGroupIndex()
+				index = await dev.policyGroup.getVotingGroupIndex()
+				expect(index.toNumber()).to.be.equal(2)
+			})
+		})
 	}
 )
-
-// TODO getVotingGroupIndex ,incrementVotingGroupIndex
