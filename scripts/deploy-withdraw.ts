109f088d5c16194b95ea3c42f4712fada77dcdd1
aggre <hiroyuki.aggre@gmail.com>
2020-09-15 15:14:05 +0900

    get the latest gas and gasPrice values asynchronously

--- scripts/deploy-withdraw.ts
+++ scripts/deploy-withdraw.ts
@@ -2,11 +2,27 @@
 import {DevCommonInstance} from './lib/instance/common'
 import {Withdraw} from './lib/instance/withdraw'
 import {WithdrawStorage} from './lib/instance/withdraw-storage'
+import {config} from 'dotenv'
+import {createFastestGasPriceFetcher} from './lib/ethgas'
+import {ethgas} from './lib/api'
+config()
+const {CONFIG: configAddress, EGS_TOKEN: egsApiKey} = process.env
 
 const handler = async (
 	callback: (err: Error | null) => void
 ): Promise<void> => {
-	const dev = new DevCommonInstance(artifacts, 4000000, 10000000000)
+	if (!configAddress || !egsApiKey) {
+		return
+	}
+
+	const gasFetcher = async () => 6721975
+	const gasPriceFetcher = createFastestGasPriceFetcher(ethgas(egsApiKey), web3)
+	const dev = new DevCommonInstance(
+		artifacts,
+		configAddress,
+		gasFetcher,
+		gasPriceFetcher
+	)
 	await dev.prepare()
 
 	// Withdraw
