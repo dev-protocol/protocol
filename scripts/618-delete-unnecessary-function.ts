447d03a6a624aa0040c232500bb9e7cc1b4954ab
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-10-19 16:05:03 +0900

    add deploy script

---
+++ scripts/618-delete-unnecessary-function.ts
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+import {createFastestGasPriceFetcher} from './lib/ethgas'
+import {ethgas} from './lib/api'
+import {config} from 'dotenv'
+import {DevCommonInstance} from './lib/instance/common'
+import {PolicyGroup} from './lib/instance/policy-group'
+
+config()
+const {CONFIG: configAddress, EGS_TOKEN: egsApiKey} = process.env
+
+const handler = async (
+	callback: (err: Error | null) => void
+): Promise<void> => {
+	if (!configAddress || !egsApiKey) {
+		return
+	}
+
+	const gasFetcher = async () => 6721975
+	const gasPriceFetcher = createFastestGasPriceFetcher(ethgas(egsApiKey), web3)
+	const dev = new DevCommonInstance(
+		artifacts,
+		configAddress,
+		gasFetcher,
+		gasPriceFetcher
+	)
+	await dev.prepare()
+
+	const policyGroup = new PolicyGroup(dev)
+	const currentPolicyGroup = await policyGroup.load()
+	const nextPolicyGroup = await policyGroup.create()
+	await policyGroup.changeOwner(currentPolicyGroup, nextPolicyGroup)
+	await policyGroup.set(nextPolicyGroup)
+
+	callback(null)
+}
+
+export = handler
