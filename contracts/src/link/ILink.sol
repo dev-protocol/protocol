32f80d42b5a5e76d0b635784579804b19f19a6b1
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-10-27 13:56:53 +0900

    update link function

--- contracts/src/link/ILink.sol
+++ contracts/src/link/ILink.sol
@@ -1,8 +1,16 @@
 pragma solidity 0.5.17;
 
 interface ILink {
+	function getTokenAddress() external view returns (address);
+
 	function getStorageLastCumulativeInterestPrice()
 		external
 		view
 		returns (uint256);
+
+	function depositFrom(address _from, address _to, uint256 _amount) external returns (bool);
+
+	function cancel(address _property) external;
+
+	function withdraw(address _property) external;
 }
--- contracts/src/link/Link.sol
+++ contracts/src/link/Link.sol
@@ -3,6 +3,7 @@ pragma solidity 0.5.17;
 import {UsingConfig} from "contracts/src/common/config/UsingConfig.sol";
 import {ILockup} from "contracts/src/lockup/ILockup.sol";
 import {ILink} from "contracts/src/link/ILink.sol";
+import {Dev} from "contracts/src/dev/Dev.sol";
 
 /**
  * This contract is an interface for calls from other systems
@@ -15,6 +16,9 @@ contract Link is UsingConfig, ILink {
 	constructor(address _config) public UsingConfig(_config) {}
 
 	/**
+	 * Lockup
+	 */
+	/**
 	 * Call the lockup contract's getStorageLastCumulativeInterestPriceLink function.
 	 */
 	function getStorageLastCumulativeInterestPrice()
@@ -24,5 +28,26 @@ contract Link is UsingConfig, ILink {
 	{
 		ILockup lockup = ILockup(config().lockup());
 		return lockup.getStorageLastCumulativeInterestPriceLink();
+	}
+
+	function cancel(address _property) external {
+		ILockup lockup = ILockup(config().lockup());
+		lockup.cancel(_property);
+	}
+
+	function withdraw(address _property) external {
+		ILockup lockup = ILockup(config().lockup());
+		lockup.withdraw(_property);
+	}
+
+	/**
+	 * Dev
+	 */
+	function getTokenAddress() external view returns (address) {
+		return config().token();
+	}
+
+	function depositFrom(address _from, address _to, uint256 _amount) external returns (bool) {
+		return Dev(config().token()).depositFrom(_from, _to, _amount);
 	}
 }
