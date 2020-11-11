0277fe64859adb7fa92963dcd3270e9024b0cf0a
Akira Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-11-11 14:30:34 +0900

    Bug fix (#637)
    * Do not perform unnecessary transfers
    
    * update by lint
    
    * add deploy script

---
+++ scripts/637-bug-fix.ts
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+import {createFastestGasPriceFetcher} from './lib/ethgas'
+import {ethgas} from './lib/api'
+import {config} from 'dotenv'
+import {DevCommonInstance} from './lib/instance/common'
+import {Lockup} from './lib/instance/lockup'
+
+config()
+const {CONFIG: configAddress, EGS_TOKEN: egsApiKey} = process.env
+
+const handler = async (
+	callback: (err: Error | null) => void
+): Promise<void> => {
+	if (!configAddress || !egsApiKey) {
+		return
+	}
+
+	const gasFetcher = async () => 6721975
+	const gasPriceFetcher = createFastestGasPriceFetcher(ethgas(egsApiKey), web3)
+	const dev = new DevCommonInstance(
+		artifacts,
+		configAddress,
+		gasFetcher,
+		gasPriceFetcher
+	)
+	await dev.prepare()
+
+	const l = new Lockup(dev)
+	const lCurrent = await l.load()
+	const lNext = await l.create()
+	await l.changeOwner(lCurrent, lNext)
+	await l.set(lNext)
+
+	callback(null)
+}
+
+export = handler
