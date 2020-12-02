94c0aaecbd58deb485a088d1f13b8e0bb9b86fd4
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-12-02 15:00:00 +0900

    update policy

--- contracts/test/policy/PolicyTestForPolicyFactory.sol
+++ contracts/test/policy/PolicyTestForPolicyFactory.sol
@@ -2,4 +2,8 @@ pragma solidity 0.5.17;
 
 import {PolicyTestBase} from "contracts/test/policy/PolicyTestBase.sol";
 
-contract PolicyTestForPolicyFactory is PolicyTestBase {}
+contract PolicyTestForPolicyFactory is PolicyTestBase {
+	function policyVotingBlocks() external view returns (uint256) {
+		return 10;
+	}
+}
