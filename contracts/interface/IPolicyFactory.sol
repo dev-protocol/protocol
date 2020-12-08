5bfb980d78dcd87a4aed4dee9c2d53f2100dc571
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2020-12-08 11:13:26 +0900

    add interface to policyFactory

--- contracts/interface/IPolicyFactory.sol
+++ contracts/interface/IPolicyFactory.sol
@@ -5,4 +5,6 @@ interface IPolicyFactory {
 	function create(address _newPolicyAddress) external;
 
 	function convergePolicy(address _currentPolicyAddress) external;
+
+	function forceAttach(address _policy) external;
 }
