0233e002b9aba9823e61d2c296351f9d54c912f8
aggre <hiroyuki.aggre@gmail.com>
2021-04-06 15:20:10 +0900

    implement post burn policy

--- contracts/src/policy/Patch780.sol
+++ contracts/src/policy/Patch780.sol
@@ -2,16 +2,12 @@
 pragma solidity 0.5.17;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {DIP1} from "contracts/src/policy/DIP1.sol";
-import {Curve} from "contracts/src/common/libs/Curve.sol";
+import {Patch662} from "contracts/src/policy/Patch662.sol";
 
-/**
- * DIP7 is a contract that changes the `rewards` of DIP1.
- */
-contract DIP7 is DIP1, Curve {
-	uint256 private constant mint_per_block_and_aseet = 120000000000000;
+contract Patch780 is Patch662 {
+	uint256 private constant mint_per_block_and_aseet = 132000000000000;
 
-	constructor(address _config) public DIP1(_config) {}
+	constructor(address _config) public Patch662(_config) {}
 
 	function rewards(uint256 _lockups, uint256 _assets)
 		external
