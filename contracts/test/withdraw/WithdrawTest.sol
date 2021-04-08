d5341dd85b2da6f011ac38226d196a3208360ff0
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-04-08 15:52:01 +0900

    update constractor

--- contracts/test/withdraw/WithdrawTest.sol
+++ contracts/test/withdraw/WithdrawTest.sol
@@ -3,7 +3,7 @@ pragma solidity 0.5.17;
 import {Withdraw} from "contracts/src/withdraw/Withdraw.sol";
 
 contract WithdrawTest is Withdraw {
-	constructor(address _config) public Withdraw(_config) {}
+	constructor(address _config, address _devMinter) public Withdraw(_config, _devMinter) {}
 
 	function setCumulativePriceTest(address _property, uint256 _value)
 		external
