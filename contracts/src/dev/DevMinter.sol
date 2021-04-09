744c90fe1df32d2790d6ac5e2558ced0e64678cb
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-04-09 09:54:37 +0900

    add pauser role

--- contracts/src/dev/DevMinter.sol
+++ contracts/src/dev/DevMinter.sol
@@ -2,10 +2,11 @@ pragma solidity 0.5.17;
 
 // prettier-ignore
 import {ERC20Mintable} from "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+import {Pausable} from "@openzeppelin/contracts/lifecycle/Pausable.sol";
 import {UsingConfig} from "contracts/src/common/config/UsingConfig.sol";
 import {IDevMinter} from "contracts/interface/IDevMinter.sol";
 
-contract DevMinter is UsingConfig, IDevMinter {
+contract DevMinter is UsingConfig, Pausable, IDevMinter {
 	/**
 	 * Initialize the passed address as AddressConfig address.
 	 */
@@ -14,7 +15,7 @@ contract DevMinter is UsingConfig, IDevMinter {
 	/**
 	 * Mint Dev token
 	 */
-	function mint(address account, uint256 amount) external returns (bool) {
+	function mint(address account, uint256 amount) external whenNotPaused returns (bool) {
 		return ERC20Mintable(config().token()).mint(account, amount);
 	}
 }
--- test/dev/dev-minter.ts
+++ test/dev/dev-minter.ts
@@ -34,22 +34,35 @@ contract('DevMinter', ([deployer, user1, user2, marketFactory, market]) => {
 	}
 
 	describe('mint', () => {
-		it('mint', async () => {
-			const dev = await createDevInstance()
-			const before = await dev.dev.balanceOf(user1)
-			expect(before.toString()).to.equal('0')
-			await dev.devMinter.mint(user1, 100)
-			const after = await dev.dev.balanceOf(user1)
-			expect(after.toString()).to.equal('100')
+		describe('success', () => {
+			it('If DevMinter has minter privileges, it can mint Dev tokens.', async () => {
+				const dev = await createDevInstance()
+				const before = await dev.dev.balanceOf(user1)
+				expect(before.toString()).to.equal('0')
+				await dev.devMinter.mint(user1, 100)
+				const after = await dev.dev.balanceOf(user1)
+				expect(after.toString()).to.equal('100')
+			})
 		})
-
-		it('not mint', async () => {
-			const devMinter = await createDevInstanceNotAddMinter()
-			const result = await devMinter.mint(user1, 100).catch((err: Error) => err)
-			validateErrorMessage(
-				result,
-				'MinterRole: caller does not have the Minter role'
-			)
+		describe('fail', () => {
+			it('If DevMinter does not has minter privileges, it can not mint Dev tokens', async () => {
+				const devMinter = await createDevInstanceNotAddMinter()
+				const result = await devMinter
+					.mint(user1, 100)
+					.catch((err: Error) => err)
+				validateErrorMessage(
+					result,
+					'MinterRole: caller does not have the Minter role'
+				)
+			})
+			it('Cannot mint Dev token when DevMinter is in pause, even if DevMinter has minter privilege.', async () => {
+				const dev = await createDevInstance()
+				await dev.devMinter.pause()
+				const result = await dev.devMinter
+					.mint(user1, 100)
+					.catch((err: Error) => err)
+				validateErrorMessage(result, 'Pausable: paused')
+			})
 		})
 	})
 })
