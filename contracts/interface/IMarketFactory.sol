6e9763967a986a195406aad96973239929207c44
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-07-21 14:31:38 +0900

    update by lint

--- contracts/interface/IMarketFactory.sol
+++ contracts/interface/IMarketFactory.sol
@@ -3,5 +3,6 @@ pragma solidity >=0.5.17;
 
 interface IMarketFactory {
 	function create(address _addr) external returns (address);
+
 	function enable(address _addr) external;
 }
--- scripts/delete-vote-counter.ts
+++ scripts/delete-vote-counter.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line capitalized-comments
 
 // addressconfig.voteCounterに0xをセッっと
-// 
+//
