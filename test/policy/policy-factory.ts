46a2fd2b055a53478dc9c11f87013977b0395ff7
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-07-26 15:43:12 +0900

    add policy factory test

--- test/policy/policy-factory.ts
+++ test/policy/policy-factory.ts
@@ -1,83 +1,131 @@
 import { IPolicyInstance } from '../../types/truffle-contracts'
 import { DevProtocolInstance } from '../test-lib/instance'
-
-import { collectsEth } from '../test-lib/utils/common'
-import { getPropertyAddress } from '../test-lib/utils/log'
+import { collectsEth, mine } from '../test-lib/utils/common'
 import {
 	validateNotOwnerErrorMessage,
 	validateAddressErrorMessage,
+	validateErrorMessage,
 } from '../test-lib/utils/error'
 
 contract(
 	'PolicyFactory',
-	([deployer, dummyPolicy, user1, propertyAuther, ...accounts]) => {
+	([deployer, dummyPolicy, user1, ...accounts]) => {
 		before(async () => {
 			await collectsEth(deployer)(accounts)
 		})
-		describe('PolicyFactory; createPolicy', () => {
+		const init = async (): Promise<
+			[DevProtocolInstance, IPolicyInstance]
+		> => {
 			const dev = new DevProtocolInstance(deployer)
-			let policy: IPolicyInstance
-			beforeEach(async () => {
-				await dev.generateAddressConfig()
-				await Promise.all([
-					dev.generatePolicyGroup(),
-					dev.generatePolicyFactory(),
-					dev.generateMarketFactory(),
-					dev.generateMarketGroup(),
-				])
-			})
+			await dev.generateAddressConfig()
+			await Promise.all([
+				dev.generatePolicyGroup(),
+				dev.generatePolicyFactory(),
+				dev.generateMarketFactory(),
+				dev.generateMarketGroup(),
+			])
+			const policy = await dev.getPolicy('PolicyTestForPolicyFactory', user1)
+			return [dev, policy]
+		}
+
+		describe('PolicyFactory; create', () => {
 			it('If the first Policy, the Policy becomes valid.', async () => {
-				policy = await dev.getPolicy('PolicyTestForPolicyFactory', user1)
+				const [dev, policy] = await init()
 				await dev.policyFactory.create(policy.address, {
 					from: user1,
 				})
 				const curentPolicyAddress = await dev.addressConfig.policy()
 				expect(curentPolicyAddress).to.be.equal(policy.address)
 			})
+
+			it('Added to the group.', async () => {
+				const [dev, policy] = await init()
+				const before = await dev.policyGroup.isGroup(policy.address)
+				expect(before).to.be.equal(false)
+				await dev.policyFactory.create(policy.address, {
+					from: user1,
+				})
+				const after = await dev.policyGroup.isGroup(policy.address)
+				expect(after).to.be.equal(true)
+			})
+			it('event was generated.', async () => {
+				const [dev, policy] = await init()
+				const result = await dev.policyFactory.create(policy.address, {
+					from: user1,
+				})
+				const event = result.logs[0].args
+				expect(event._from).to.be.equal(user1)
+				expect(event._policy).to.be.equal(policy.address)
+			})
+			it('If the policy already exists, it will not be applied.', async () => {
+				const [dev, policy] = await init()
+				await dev.policyFactory.create(policy.address, {
+					from: user1,
+				})
+				const secoundPolicy = await dev.getPolicy('PolicyTestForPolicyFactory', user1)
+				const before = await dev.policyGroup.isGroup(secoundPolicy.address)
+				expect(before).to.be.equal(false)
+				await dev.policyFactory.create(secoundPolicy.address, {
+					from: user1,
+				})
+				const curentPolicyAddress = await dev.addressConfig.policy()
+				expect(curentPolicyAddress).to.be.equal(policy.address)
+				const after = await dev.policyGroup.isGroup(secoundPolicy.address)
+				expect(after).to.be.equal(true)
+			})
 		})
 		describe('PolicyFactory; forceAttach', () => {
-			const dev = new DevProtocolInstance(deployer)
-			let createdPropertyAddress: string
-			beforeEach(async () => {
-				await dev.generateAddressConfig()
-				await dev.generateDev()
-				await dev.generateDevMinter()
-				await Promise.all([
-					dev.generatePolicyGroup(),
-					dev.generatePolicyFactory(),
-					dev.generateMarketFactory(),
-					dev.generateMarketGroup(),
-					dev.generatePropertyGroup(),
-					dev.generatePropertyFactory(),
-					dev.generateLockup(),
-					dev.generateAllocator(),
-					dev.generateWithdraw(),
-					dev.generateMetricsGroup(),
-				])
-				await dev.dev.mint(user1, 10000, { from: deployer })
-				await dev.generatePolicy('PolicyTestForPolicyFactory')
-				const propertyCreateResult = await dev.propertyFactory.create(
-					'test',
-					'TST',
-					propertyAuther
-				)
-				createdPropertyAddress = getPropertyAddress(propertyCreateResult)
-				await dev.metricsGroup.__setMetricsCountPerProperty(
-					createdPropertyAddress,
-					1
-				)
+			describe('failed', () => {
+				it('can not be performed by anyone other than the owner.', async () => {
+					const [dev] = await init()
+					const result = await dev.policyFactory
+						.forceAttach(dummyPolicy, { from: user1 })
+						.catch((err: Error) => err)
+					validateNotOwnerErrorMessage(result)
+				})
+				it('can not specify anything other than policy.', async () => {
+					const [dev] = await init()
+					const result = await dev.policyFactory
+						.forceAttach(dummyPolicy)
+						.catch((err: Error) => err)
+					validateAddressErrorMessage(result)
+				})
+				it('deadline is over.', async () => {
+					const [dev, policy] = await init()
+					await dev.policyFactory.create(policy.address, {
+						from: user1,
+					})
+					const secoundPolicy = await dev.getPolicy('PolicyTestForPolicyFactory', user1)
+					await dev.policyFactory.create(secoundPolicy.address, {
+						from: user1,
+					})
+					let curentPolicyAddress = await dev.addressConfig.policy()
+					expect(curentPolicyAddress).to.be.equal(policy.address)
+					await mine(10)
+					const result = await dev.policyFactory.forceAttach(secoundPolicy.address).catch((err: Error) => err)
+					validateErrorMessage(result, 'deadline is over')
+					curentPolicyAddress = await dev.addressConfig.policy()
+					expect(curentPolicyAddress).to.be.equal(policy.address)
+				})
 			})
-			it('can not be performed by anyone other than the owner.', async () => {
-				const result = await dev.policyFactory
-					.forceAttach(dummyPolicy, { from: user1 })
-					.catch((err: Error) => err)
-				validateNotOwnerErrorMessage(result)
-			})
-			it('can not specify anything other than policy.', async () => {
-				const result = await dev.policyFactory
-					.forceAttach(dummyPolicy)
-					.catch((err: Error) => err)
-				validateAddressErrorMessage(result)
+			describe('success', () => {
+				it('policy is force attach.', async () => {
+					const [dev, policy] = await init()
+					await dev.policyFactory.create(policy.address, {
+						from: user1,
+					})
+					const secoundPolicy = await dev.getPolicy('PolicyTestForPolicyFactory', user1)
+					await dev.policyFactory.create(secoundPolicy.address, {
+						from: user1,
+					})
+					let curentPolicyAddress = await dev.addressConfig.policy()
+					expect(curentPolicyAddress).to.be.equal(policy.address)
+
+					await dev.policyFactory.forceAttach(secoundPolicy.address)
+
+					curentPolicyAddress = await dev.addressConfig.policy()
+					expect(curentPolicyAddress).to.be.equal(secoundPolicy.address)
+				})
 			})
 		})
 	}
