0424b091afd066abd5080c1b3988dcb11a27415b
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-09-10 15:51:56 +0900

    update typo

--- contracts/test/token/STokenManagerTest.sol
+++ contracts/test/token/STokenManagerTest.sol
@@ -43,7 +43,7 @@ contract STokensManagerTest is ERC721 {
 		address _property,
 		uint256 _amount,
 		uint256 _price
-	) external onlyLockup returns (uint256 tikenId_) {
+	) external onlyLockup returns (uint256 tokenId_) {
 		_tokenIds.increment();
 		uint256 newTokenId = _tokenIds.current();
 		_safeMint(_owner, newTokenId);
@@ -60,21 +60,21 @@ contract STokensManagerTest is ERC721 {
 	}
 
 	function update(
-		uint256 _tikenId,
+		uint256 _tokenId,
 		uint256 _amount,
 		uint256 _price,
 		uint256 _cumulativeReward,
 		uint256 _pendingReward
 	) external onlyLockup returns (bool) {
-		require(_exists(_tikenId), "not found");
+		require(_exists(_tokenId), "not found");
 		StakingPositionV1 memory currentPosition = getStoragePositionsV1(
-			_tikenId
+			_tokenId
 		);
 		currentPosition.amount = _amount;
 		currentPosition.price = _price;
 		currentPosition.cumulativeReward = _cumulativeReward;
 		currentPosition.pendingReward = _pendingReward;
-		setStoragePositionsV1(_tikenId, currentPosition);
+		setStoragePositionsV1(_tokenId, currentPosition);
 		return true;
 	}
 
