75b5399afa8ef157ba398e7e9bc3569fdebbf66d
0xkenta <kenta0558kiichi614@gmail.com>
2021-11-30 18:48:11 +0100

    add type for deployedContract and eslint-disable-next-line

--- test/test-lib/utils/event.ts
+++ test/test-lib/utils/event.ts
@@ -1,4 +1,5 @@
 import Web3 from 'web3'
+import { Contract } from 'web3-eth-contract'
 import { AbiItem } from 'web3-utils'
 
 export const watch =
@@ -7,14 +8,11 @@ export const watch =
 		name: string,
 		handler: (err: Error, values: Record<string, string>) => void
 	): void => {
-		const deployed = deployedContract as {
-			_jsonInterface: AbiItem[]
-			_address: string
-		}
+		const { contract: deployed } = deployedContract as {contract: {_jsonInterface: AbiItem[], _address: string}}
 		const web3WithWebsockets = new Web3(
 			new Web3.providers.WebsocketProvider(web3.eth.currentProvider.host)
 		)
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+		// eslint-disable-next-line
 		const { events } = new web3WithWebsockets.eth.Contract(
 			deployed._jsonInterface,
 			deployed._address
